d1345d693181fd5472d3d5badf6d876a8023324a
vuongkienthanh <thanhstardust@outlook.com>
2023-12-14 13:00:38 +0700

    Asd

--- 2023/day07/src/lib.rs
+++ 2023/day07/src/lib.rs
@@ -15,104 +15,102 @@ impl Card {
     }
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+#[repr(u8)]
 pub enum HandType {
-    // same as sorted hand
-    HighCard([Card; 5]),
-    // [pair, a, b, c],
-    OnePair([Card; 4]),
-    // [pair1, pair2, a] pair1>pair2
-    TwoPair([Card; 3]),
-    // [three, a, b]
-    ThreeOfAKind([Card; 3]),
-    // [three, pair]
-    FullHouse([Card; 2]),
-    // [four, a]
-    FourOfAKind([Card; 2]),
-    // single card
-    FiveOfAKind(Card),
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
 }
-impl HandType {
-    pub fn strength(&self) -> usize {
-        match self {
-            HandType::HighCard(_) => 1,
-            HandType::OnePair(_) => 2,
-            HandType::TwoPair(_) => 3,
-            HandType::ThreeOfAKind(_) => 4,
-            HandType::FullHouse(_) => 5,
-            HandType::FourOfAKind(_) => 6,
-            HandType::FiveOfAKind(_) => 7,
-        }
-    }
-}
-impl PartialOrd for HandType {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.strength()
-            .partial_cmp(&other.strength())
-            .and_then(|ordering| match ordering {
-                Ordering::Equal => todo!(),
-                order => Some(order),
-            })
-    }
-}
+
+#[derive(PartialEq, Eq, Ord, Clone, Copy)]
+pub struct Hand([Card; 5]);
+#[derive(PartialEq, Eq, Ord, Clone, Copy)]
 pub struct SortedHand([Card; 5]);
 
-impl SortedHand {
+impl Hand {
     pub fn from(src: &str) -> Self {
         assert!(src.len() == 5, "hand length should be 5");
         let mut cards = src.chars().map(|c| Card::from(c));
-        let mut cards = [
+        let cards = [
             cards.next().unwrap(),
             cards.next().unwrap(),
             cards.next().unwrap(),
             cards.next().unwrap(),
             cards.next().unwrap(),
         ];
-        cards.sort_unstable_by(|a, b| b.cmp(a));
         Self(cards)
     }
 
-    pub fn hand_type(&self) -> HandType {
-        // five of a kind
+    pub fn to_sorted(&self) -> SortedHand {
+        let mut sorted_cards = self.0.clone();
+        sorted_cards.sort_unstable_by(|a, b| b.cmp(a));
+        SortedHand(sorted_cards)
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.hand_type()
+            .partial_cmp(&other.hand_type())
+            .and_then(|ordering| match ordering {
+                Ordering::Equal => self.0.partial_cmp(&other.0),
+                other => Some(other),
+            })
+    }
+}
+impl PartialOrd for SortedHand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.hand_type()
+            .partial_cmp(&other.hand_type())
+            .and_then(|ordering| match ordering {
+                Ordering::Equal => self.0.partial_cmp(&other.0),
+                other => Some(other),
+            })
+    }
+}
+
+pub trait ToHandType {
+    fn hand_type(&self) -> HandType;
+}
+impl ToHandType for Hand {
+    fn hand_type(&self) -> HandType {
+        self.to_sorted().hand_type()
+    }
+}
+
+impl ToHandType for SortedHand {
+    fn hand_type(&self) -> HandType {
         if self.0.iter().take(4).all(|c| c == &self.0[4]) {
-            HandType::FiveOfAKind(self.0[0])
-        // four of a kind
-        } else if self.0.iter().take(3).all(|c| c == &self.0[3]) {
-            HandType::FourOfAKind([self.0[0], self.0[4]])
-        } else if self.0.iter().skip(1).take(3).all(|c| c == &self.0[4]) {
-            HandType::FourOfAKind([self.0[4], self.0[0]])
-        // full house
+            HandType::FiveOfAKind
+        } else if self.0.iter().take(3).all(|c| c == &self.0[3])
+            || self.0.iter().skip(1).take(3).all(|c| c == &self.0[4])
+        {
+            HandType::FourOfAKind
         } else if self.0[0] == self.0[1] && self.0[3] == self.0[4] {
-            match self.0[2] == self.0[0] {
-                true => HandType::FullHouse([self.0[2], self.0[4]]),
-                false => HandType::FullHouse([self.0[2], self.0[0]]),
-            }
-        // three of a kind
-        } else if self.0.iter().take(2).all(|c| c == &self.0[2]) {
-            HandType::ThreeOfAKind([self.0[0], self.0[3], self.0[4]])
-        } else if self.0.iter().skip(1).take(2).all(|c| c == &self.0[3]) {
-            HandType::ThreeOfAKind([self.0[1], self.0[0], self.0[4]])
-        } else if self.0.iter().skip(2).take(2).all(|c| c == &self.0[4]) {
-            HandType::ThreeOfAKind([self.0[2], self.0[0], self.0[1]])
-        // two pair
-        } else if self.0[0] == self.0[1] && self.0[2] == self.0[3] {
-            HandType::TwoPair([self.0[0], self.0[2], self.0[4]])
-        } else if self.0[0] == self.0[1] && self.0[3] == self.0[4] {
-            HandType::TwoPair([self.0[0], self.0[3], self.0[2]])
-        } else if self.0[1] == self.0[2] && self.0[3] == self.0[4] {
-            HandType::TwoPair([self.0[1], self.0[3], self.0[0]])
-        // one pair
-        } else if self.0[0] == self.0[1] {
-            HandType::OnePair([self.0[0], self.0[2], self.0[3], self.0[4]])
-        } else if self.0[1] == self.0[2] {
-            HandType::OnePair([self.0[1], self.0[0], self.0[3], self.0[4]])
-        } else if self.0[2] == self.0[3] {
-            HandType::OnePair([self.0[2], self.0[0], self.0[1], self.0[4]])
-        } else if self.0[3] == self.0[4] {
-            HandType::OnePair([self.0[3], self.0[0], self.0[1], self.0[2]])
-        // high card
+            HandType::FullHouse
+        } else if self.0.iter().take(2).all(|c| c == &self.0[2])
+            || self.0.iter().skip(1).take(2).all(|c| c == &self.0[3])
+            || self.0.iter().skip(2).take(2).all(|c| c == &self.0[4])
+        {
+            HandType::ThreeOfAKind
+        } else if (self.0[0] == self.0[1] && self.0[2] == self.0[3])
+            || (self.0[0] == self.0[1] && self.0[3] == self.0[4])
+            || (self.0[1] == self.0[2] && self.0[3] == self.0[4])
+        {
+            HandType::TwoPair
+        } else if self.0[0] == self.0[1]
+            || self.0[1] == self.0[2]
+            || self.0[2] == self.0[3]
+            || self.0[3] == self.0[4]
+        {
+            HandType::OnePair
         } else {
-            HandType::HighCard(self.0)
+            HandType::HighCard
         }
     }
 }
--- 2023/day07/src/part1.rs
+++ 2023/day07/src/part1.rs
@@ -1,13 +1,33 @@
-use crate::Card;
+use crate::Hand;
 pub fn process(_input: &str) -> usize {
-    todo!("part1")
+    let mut pairs = _input
+        .lines()
+        .map(|line| {
+            let mut linesplit = line.split_ascii_whitespace();
+            let hand = Hand::from(linesplit.next().unwrap());
+            let score = linesplit.next().unwrap().parse::<usize>().unwrap();
+            (hand, score)
+        })
+        .collect::<Vec<_>>();
+    pairs.sort_by_key(|&(hand, _score)| hand);
+    dbg!(&pairs);
+    pairs
+        .iter()
+        .enumerate()
+        .map(|(i, (_cards, score))| (i + 1) * score)
+        .sum();
+    0
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_process() {
-        let input = r#""#;
-        assert_eq!(process(input), 0);
+        let input = r#"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"#;
+        assert_eq!(process(input), 6440);
     }
 }
