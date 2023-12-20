e2b24edecff7f9852deecea9f8dfe33e4e92b8c6
vuongkienthanh <thanhstardust@outlook.com>
2023-12-20 19:45:51 +0700

    day11

--- 2023/day11/src/part1.rs
+++ 2023/day11/src/part1.rs
@@ -1,77 +1,8 @@
-use itertools::Itertools;
+use crate::GalaxyMap;
 
-fn expand_input(input: &str) -> Vec<Vec<char>> {
-    let row_count = input.lines().count();
-    let col_count = input.lines().next().unwrap().len();
-
-    let expand_row_indices = (0..row_count)
-        .filter_map(|i| {
-            if input.lines().nth(i).unwrap().chars().all(|c| c == '.') {
-                Some(i)
-            } else {
-                None
-            }
-        })
-        .rev()
-        .collect::<Vec<usize>>();
-
-    let expand_col_indices = (0..col_count)
-        .filter_map(|j| {
-            if input
-                .lines()
-                .map(|line| line.chars().nth(j).unwrap())
-                .all(|c| c == '.')
-            {
-                Some(j)
-            } else {
-                None
-            }
-        })
-        .rev()
-        .collect::<Vec<usize>>();
-
-    let mut expanded = vec![];
-
-    for i in 0..row_count {
-        let mut line = input.lines().nth(i).unwrap().chars().collect::<Vec<char>>();
-        for idx in &expand_col_indices {
-            line.insert(*idx, '.');
-        }
-        expanded.push(line);
-    }
-
-    let insert_row = (0..(col_count + expand_col_indices.len()))
-        .into_iter()
-        .map(|_| '.')
-        .collect::<Vec<char>>();
-
-    for i in expand_row_indices {
-        expanded.insert(i, insert_row.clone());
-    }
-    expanded
-}
 pub fn process(_input: &str) -> usize {
-    let expanded = expand_input(_input);
-
-    let row_count = expanded.len();
-    let col_count = expanded.get(0).unwrap().len();
-
-    let galaxies = (0..row_count)
-        .into_iter()
-        .flat_map(|i| (0..col_count).into_iter().map(move |j| (i, j)))
-        .filter(|(i, j)| expanded.get(*i).unwrap().get(*j).unwrap() == &'#')
-        .collect::<Vec<(usize, usize)>>();
-
-    galaxies
-        .into_iter()
-        .combinations(2)
-        .map(|pair| {
-            let p0 = pair.get(0).unwrap();
-            let p1 = pair.get(1).unwrap();
-
-            p1.0.abs_diff(p0.0) + p1.1.abs_diff(p0.1)
-        })
-        .sum()
+    let mut galaxies = GalaxyMap::new(_input);
+    galaxies.solve(2)
 }
 #[cfg(test)]
 mod tests {
--- 2023/day11/src/part2.rs
+++ 2023/day11/src/part2.rs
@@ -1,12 +1,15 @@
+use crate::GalaxyMap;
+
 pub fn process(_input: &str) -> usize {
-    todo!("part2")
+    let mut galaxies = GalaxyMap::new(_input);
+    galaxies.solve(1_000_000)
 }
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    #[ignore]
-    fn test_process() {
+    fn test_process_10_times() {
         let input = r#"...#......
 .......#..
 #.........
@@ -17,6 +20,23 @@ mod tests {
 ..........
 .......#..
 #...#....."#;
-        assert_eq!(process(input), 0);
+        let mut galaxies = GalaxyMap::new(input);
+        assert_eq!(galaxies.solve(10), 1030);
+    }
+
+    #[test]
+    fn test_process_100_times() {
+        let input = r#"...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."#;
+        let mut galaxies = GalaxyMap::new(input);
+        assert_eq!(galaxies.solve(100), 8410);
     }
 }
