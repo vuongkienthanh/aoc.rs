de1baed004808cdb240ba02b926fac7a54a8bb5d
vuongkienthanh <thanhstardust@outlook.com>
2023-12-25 15:58:54 +0700

    day13

--- 2023/day13/src/part2.rs
+++ 2023/day13/src/part2.rs
@@ -1,3 +1,10 @@
+fn two_vec_char_diff(a: &[char], b: &[char]) -> Vec<usize> {
+    a.iter()
+        .zip(b.iter())
+        .enumerate()
+        .filter_map(|(i, (x, y))| if x != y { Some(i) } else { None })
+        .collect::<Vec<_>>()
+}
 pub fn process(_input: &str) -> usize {
     todo!("part2")
 }
@@ -6,7 +13,21 @@ mod tests {
     use super::*;
     #[test]
     fn test_process() {
-        let input = r#""#;
-        assert_eq!(process(input), 0);
+        let input = r#"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#"#;
+        assert_eq!(process(input), 400);
     }
 }
