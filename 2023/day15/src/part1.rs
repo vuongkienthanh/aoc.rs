059d6d20659b4a44f850db4d05361cebf5d7944d
vuongkienthanh <thanhstardust@outlook.com>
2023-12-26 16:05:45 +0700

    dasf

--- 2023/day15/src/part1.rs
+++ 2023/day15/src/part1.rs
@@ -1,12 +1,20 @@
 pub fn process(_input: &str) -> usize {
-    todo!("part1")
+    let input = _input.replace("\n", "");
+    input
+        .split(',')
+        .map(|group| {
+            group
+                .chars()
+                .fold(0, |acc, c| (acc + c as usize) * 17 % 256)
+        })
+        .sum()
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_process() {
-        let input = r#""#;
-        assert_eq!(process(input), 0);
+        let input = r#"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"#;
+        assert_eq!(process(input), 1320);
     }
 }
--- 2023/day15/src/part2.rs
+++ 2023/day15/src/part2.rs
@@ -1,12 +1,41 @@
-pub fn process(_input: &str) -> usize {
-    todo!("part2")
+use std::collections::HashMap;
+
+struct Lens <'a>{
+    label: &'a str,
+    focus: usize,
 }
+
+impl<'a> Lens<'a> {
+    fn new(label: &str, focus: usize) -> Self {
+        if focus<1 || focus >9 {
+            panic!("focus out of range");
+        }
+        Self {
+            label,
+            focus,
+        }
+    }
+}
+
+pub fn process(_input: &str) -> usize {
+    let input = _input.replace("\n", "");
+    let mut boxes : HashMap<usize, Vec<Lens>> = HashMap::new();
+    // for step in input.split(',') {
+    //     match step.chars().last().unwrap() {
+    //         '-' => todo!("dash"),
+    //         equal_sign_step => todo!("equal"),
+    //     }
+    // }
+
+    boxes.iter()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_process() {
-        let input = r#""#;
-        assert_eq!(process(input), 0);
+        let input = r#"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"#;
+        assert_eq!(process(input), 145);
     }
 }
