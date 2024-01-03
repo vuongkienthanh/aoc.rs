0d52bceee70d6b2f74233a4e61f35d3b9ae1d311
vuongkienthanh <thanhstardust@outlook.com>
2024-01-03 22:42:05 +0700

    asd

--- 2023/day19/src/part1.rs
+++ 2023/day19/src/part1.rs
@@ -1,12 +1,149 @@
-pub fn process(_input: &str) -> usize {
-    todo!("part1")
+use predicates::prelude::*;
+use std::collections::HashMap;
+use crate::{PredicationResult, MachinePartAttribute};
+
+#[derive(Debug)]
+struct Workflow {
+    predications: Vec<Predication>,
+}
+impl Workflow {
+    fn eval(&self, mp: &MachinePart) -> &PredicationResult {
+        for p in self.predications.iter() {
+            let attr = match p.attr {
+                Some(MachinePartAttribute::X) => mp.x,
+                Some(MachinePartAttribute::M) => mp.m,
+                Some(MachinePartAttribute::A) => mp.a,
+                Some(MachinePartAttribute::S) => mp.s,
+                _ => 0,
+            };
+            if p.predication.eval(&attr) {
+                return &p.result;
+            }
+        }
+        panic!("should always return")
+    }
+}
+
+#[derive(Debug)]
+struct Predication {
+    attr: Option<MachinePartAttribute>,
+    predication: predicates::BoxPredicate<usize>,
+    result: PredicationResult,
+}
+
+
+#[derive(Debug)]
+struct MachinePart {
+    x: usize,
+    m: usize,
+    a: usize,
+    s: usize,
+}
+impl MachinePart {
+    fn go_through<'a>(&self, system: &'a HashMap<String, Workflow>) -> &'a PredicationResult {
+        let mut workflow = system.get("in").unwrap();
+        loop {
+            match workflow.eval(self) {
+                PredicationResult::Refer(name) => workflow = system.get(name).unwrap(),
+                other => return other,
+            }
+        }
+    }
+    fn rating(&self) -> usize {
+        self.x + self.m + self.a + self.s
+    }
+}
+
+fn parse_input(input: &str) -> (HashMap<String, Workflow>, Vec<MachinePart>) {
+    let (workflows, parts) = input.split_once("\n\n").unwrap();
+
+    let mut system = HashMap::new();
+    for line in workflows.lines() {
+        let name = line.chars().take_while(|c| *c != '{').collect::<String>();
+        let predications = line
+            .chars()
+            .skip_while(|c| *c != '{')
+            .skip(1)
+            .take_while(|c| *c != '}')
+            .collect::<String>()
+            .split(',')
+            .map(|p| {
+                if p.contains(':') {
+                    let attr = Some(MachinePartAttribute::new(p.chars().take(1).next().unwrap()));
+                    let sign = p.chars().skip(1).take(1).next().unwrap();
+                    let (compare_value, result) = &p[2..].split_once(':').unwrap();
+                    let compare_value = compare_value.parse::<usize>().unwrap();
+                    let predication = match sign {
+                        '>' => predicate::gt(compare_value).boxed(),
+                        '<' => predicate::lt(compare_value).boxed(),
+                        _ => panic!("not < or >"),
+                    };
+                    let result = PredicationResult::new(result);
+                    Predication {
+                        attr,
+                        predication,
+                        result,
+                    }
+                } else {
+                    let attr = None;
+                    let predication = predicate::always().boxed();
+                    let result = PredicationResult::new(p);
+                    Predication {
+                        attr,
+                        predication,
+                        result,
+                    }
+                }
+            })
+            .collect::<Vec<_>>();
+        system.insert(name, Workflow { predications });
+    }
+
+    let machineparts = parts
+        .lines()
+        .map(|line| {
+            let mut split = line[1..line.len() - 1].split(',');
+            let x = split.next().unwrap()[2..].parse::<usize>().unwrap();
+            let m = split.next().unwrap()[2..].parse::<usize>().unwrap();
+            let a = split.next().unwrap()[2..].parse::<usize>().unwrap();
+            let s = split.next().unwrap()[2..].parse::<usize>().unwrap();
+            MachinePart { x, m, a, s }
+        })
+        .collect::<Vec<_>>();
+
+    (system, machineparts)
+}
+
+pub fn process(input: &str) -> usize {
+    let (system, machineparts) = parse_input(input);
+    machineparts
+        .into_iter()
+        .filter(|mp| mp.go_through(&system) == &PredicationResult::Accept)
+        .map(|mp| mp.rating())
+        .sum()
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_process() {
-        let input = r#""#;
-        assert_eq!(process(input), 0);
+        let input = r#"px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}"#;
+        assert_eq!(process(input), 19114);
     }
 }
--- 2023/day19/src/part2.rs
+++ 2023/day19/src/part2.rs
@@ -1,12 +1,108 @@
-pub fn process(_input: &str) -> usize {
-    todo!("part2")
+use crate::{CompareSign, MachinePartAttribute, PredicationResult};
+use std::collections::HashMap;
+use std::ops::Range;
+
+#[derive(Debug)]
+struct Predicate {
+    attr: MachinePartAttribute,
+    sign: CompareSign,
+    value: usize,
+}
+
+#[derive(Debug)]
+struct Workflow {
+    predication: Option<Predicate>,
+    result: PredicationResult,
+}
+
+fn parse_input(input: &str) -> HashMap<String, Vec<Workflow>> {
+    input
+        .split_once("\n\n")
+        .unwrap()
+        .0
+        .lines()
+        .map(|line| {
+            let name = line.chars().take_while(|c| *c != '{').collect::<String>();
+
+            let workflows = line
+                .chars()
+                .skip_while(|c| *c != '{')
+                .skip(1)
+                .take_while(|c| *c != '}')
+                .collect::<String>()
+                .split(',')
+                .map(|p| {
+                    if p.contains(':') {
+                        let attr = MachinePartAttribute::new(p.chars().take(1).next().unwrap());
+                        let sign = CompareSign::new(p.chars().skip(1).take(1).next().unwrap());
+                        let (compare_value, result) = &p[2..].split_once(':').unwrap();
+                        let value = compare_value.parse::<usize>().unwrap();
+                        let predication = Some(Predicate { attr, sign, value });
+                        let result = PredicationResult::new(result);
+                        Workflow {
+                            predication,
+                            result,
+                        }
+                    } else {
+                        let result = PredicationResult::new(p);
+                        Workflow {
+                            predication: None,
+                            result,
+                        }
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            (name, workflows)
+        })
+        .collect()
+}
+
+struct State {
+    x: Range<usize>,
+    m: Range<usize>,
+    a: Range<usize>,
+    s: Range<usize>,
+    dst: PredicationResult,
+}
+
+impl State {
+    fn go_through(&self, workflows: &[Workflow]) -> Vec<State> {
+        let mut ret = vec![];
+        for wf in workflows {
+
+        }
+        ret
+    }
+}
+
+pub fn process(input: &str) -> usize {
+    let system = parse_input(input);
+    dbg!(system);
+    0
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_process() {
-        let input = r#""#;
-        assert_eq!(process(input), 0);
+        let input = r#"px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}"#;
+        assert_eq!(process(input), 167409079868000);
     }
 }
