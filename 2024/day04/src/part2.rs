66db929d9df572bf7310aba87cff2fb39207f533
vuongkienthanh <thanhstardust@outlook.com>
2024-12-04 16:12:14 +0700

    day4

--- 2024/day04/src/part2.rs
+++ 2024/day04/src/part2.rs
@@ -1,12 +1,63 @@
+use grid::Grid;
+
 pub fn process(_input: &str) -> usize {
-    todo!("part2")
+    let grid = Grid::from(
+        _input
+            .lines()
+            .map(|line| line.chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>(),
+    );
+
+    (1..grid.rows() - 1)
+        .flat_map(|i| (1..grid.cols() - 1).map(move |j| (i, j)))
+        .filter(|(row, col)| grid.get(*row, *col).unwrap() == &'A')
+        .flat_map(|(row, col)| {
+            [
+                [
+                    (row - 1, col - 1),
+                    (row - 1, col + 1),
+                    (row + 1, col + 1),
+                    (row + 1, col - 1),
+                ],
+                [
+                    (row - 1, col + 1),
+                    (row + 1, col + 1),
+                    (row + 1, col - 1),
+                    (row - 1, col - 1),
+                ],
+                [
+                    (row + 1, col + 1),
+                    (row + 1, col - 1),
+                    (row - 1, col - 1),
+                    (row - 1, col + 1),
+                ],
+                [
+                    (row + 1, col - 1),
+                    (row - 1, col - 1),
+                    (row - 1, col + 1),
+                    (row + 1, col + 1),
+                ],
+            ]
+        })
+        .map(|v| v.map(|(row, col)| grid.get(row, col).unwrap()))
+        .filter(|v| v == &[&'M', &'S', &'S', &'M'])
+        .count()
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_process() {
-        let input = r#""#;
-        assert_eq!(process(input), 0);
+        let input = r#"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"#;
+        assert_eq!(process(input), 9);
     }
 }
