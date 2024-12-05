741ee73262ac968376e950bf531d861ccf08dc71
vuongkienthanh <thanhstardust@outlook.com>
2024-12-05 07:25:07 +0700

    update day4

--- 2024/day04/src/part2.rs
+++ 2024/day04/src/part2.rs
@@ -11,36 +11,40 @@ pub fn process(_input: &str) -> usize {
     (1..grid.rows() - 1)
         .flat_map(|i| (1..grid.cols() - 1).map(move |j| (i, j)))
         .filter(|(row, col)| grid.get(*row, *col).unwrap() == &'A')
-        .flat_map(|(row, col)| {
+        .filter(|(row, col)| {
             [
                 [
                     (row - 1, col - 1),
                     (row - 1, col + 1),
                     (row + 1, col + 1),
                     (row + 1, col - 1),
-                ],
+                ]
+                .map(|(row, col)| grid.get(row, col).unwrap()),
                 [
                     (row - 1, col + 1),
                     (row + 1, col + 1),
                     (row + 1, col - 1),
                     (row - 1, col - 1),
-                ],
+                ]
+                .map(|(row, col)| grid.get(row, col).unwrap()),
                 [
                     (row + 1, col + 1),
                     (row + 1, col - 1),
                     (row - 1, col - 1),
                     (row - 1, col + 1),
-                ],
+                ]
+                .map(|(row, col)| grid.get(row, col).unwrap()),
                 [
                     (row + 1, col - 1),
                     (row - 1, col - 1),
                     (row - 1, col + 1),
                     (row + 1, col + 1),
-                ],
+                ]
+                .map(|(row, col)| grid.get(row, col).unwrap()),
             ]
+            .into_iter()
+            .any(|v| v == [&'M', &'S', &'S', &'M'])
         })
-        .map(|v| v.map(|(row, col)| grid.get(row, col).unwrap()))
-        .filter(|v| v == &[&'M', &'S', &'S', &'M'])
         .count()
 }
 #[cfg(test)]
