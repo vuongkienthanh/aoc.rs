b7d3a81983601bf1fbf7e8c5ba5b8e4bb2ef2241
vuongkienthanh <thanhstardust@outlook.com>
2024-12-06 06:37:28 +0700

    update day 05

--- 2024/day05/src/lib.rs
+++ 2024/day05/src/lib.rs
@@ -3,10 +3,10 @@ pub mod part2;
 
 use nom::{
     bytes::complete::tag,
-    character::complete::{digit1, newline},
-    multi::{count, separated_list1},
-    sequence::separated_pair,
-    IResult, Parser,
+    character::complete::{digit1, line_ending},
+    multi::{fold_many1, separated_list1},
+    sequence::{separated_pair, terminated},
+    IResult,
 };
 use std::cmp::Ordering;
 use std::collections::HashMap;
@@ -20,23 +20,23 @@ fn rule(input: &str) -> IResult<&str, Rule> {
     separated_pair(digit1, tag("|"), digit1)(input)
 }
 fn rules(input: &str) -> IResult<&str, Rules> {
-    separated_list1(newline, rule)
-        .map(|rs| {
-            rs.into_iter().fold(Rules::new(), |mut acc, (a, b)| {
-                acc.entry(a).or_default().insert(b, Ordering::Less);
-                acc.entry(b).or_default().insert(a, Ordering::Greater);
-                acc
-            })
-        })
-        .parse(input)
+    fold_many1(
+        terminated(rule, line_ending),
+        Rules::new,
+        |mut acc, (a, b)| {
+            acc.entry(a).or_default().insert(b, Ordering::Less);
+            acc.entry(b).or_default().insert(a, Ordering::Greater);
+            acc
+        },
+    )(input)
 }
 fn update(input: &str) -> IResult<&str, Update> {
     separated_list1(tag(","), digit1)(input)
 }
 fn updates(input: &str) -> IResult<&str, Updates> {
-    separated_list1(newline, update)(input)
+    separated_list1(line_ending, update)(input)
 }
 
 pub fn parse(input: &str) -> IResult<&str, (Rules, Updates)> {
-    separated_pair(rules, count(newline, 2), updates)(input)
+    separated_pair(rules, line_ending, updates)(input)
 }
