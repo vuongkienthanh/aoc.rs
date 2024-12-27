787010983afebfb9e36606acd6954e21cb0f0190
vuongkienthanh <thanhstardust@outlook.com>
2024-12-27 16:10:09 +0700

    finsih 22

--- 2024/day22/src/part1.rs
+++ 2024/day22/src/part1.rs
@@ -1,12 +1,26 @@
+use crate::evolve;
 pub fn process(_input: &str) -> usize {
-    todo!("part1")
+    _input
+        .lines()
+        .map(|x| x.parse::<usize>().unwrap())
+        .map(|mut x| {
+            for _ in 0..2000 {
+                x = evolve(x)
+            }
+            x
+        })
+        .sum()
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
+    #[ignore = "reason"]
     fn test_process() {
-        let input = r#""#;
-        assert_eq!(process(input), 0);
+        let input = r#"1
+10
+100
+2024"#;
+        assert_eq!(process(input), 37327623);
     }
 }
--- 2024/day22/src/part2.rs
+++ 2024/day22/src/part2.rs
@@ -1,12 +1,47 @@
+use crate::evolve;
+use std::collections::{HashMap, HashSet};
+
 pub fn process(_input: &str) -> usize {
-    todo!("part2")
+    _input
+        .lines()
+        .map(|x| x.parse::<usize>().unwrap())
+        .fold(HashMap::new(), |mut acc, mut x| {
+            let mut digit = (x % 10) as isize;
+            let mut changes = [0; 4];
+            for i in &mut changes {
+                let new_x = evolve(x);
+                let new_digit = (new_x % 10) as isize;
+                *i = new_digit - digit;
+                x = new_x;
+                digit = new_digit;
+            }
+            let mut seen = HashSet::new();
+            for _ in 0..1996 {
+                let new_x = evolve(x);
+                let new_digit = (new_x % 10) as isize;
+                changes = [changes[1], changes[2], changes[3], new_digit - digit];
+
+                if seen.insert(changes) {
+                    *acc.entry(changes).or_default() += new_digit as usize;
+                }
+                x = new_x;
+                digit = new_digit;
+            }
+            acc
+        })
+        .into_values()
+        .max()
+        .unwrap()
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_process() {
-        let input = r#""#;
-        assert_eq!(process(input), 0);
+        let input = r#"1
+2
+3
+2024"#;
+        assert_eq!(process(input), 23);
     }
 }
