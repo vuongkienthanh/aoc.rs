b77c0ec86d0df33c368d6bde3415f1f3b782deb7
vuongkienthanh <thanhstardust@outlook.com>
2025-11-20 21:36:50 +0700

    day9

--- 2016/day09/src/part1.rs
+++ 2016/day09/src/part1.rs
@@ -1,29 +1,50 @@
-use crate::parsing::parse_input;
-
 pub fn process(_input: &str) -> usize {
-    let (_rest, input) = parse_input(_input).unwrap();
-    assert!(_rest.is_empty());
-    println!("{input:?}");
-
-    todo!("part1")
+    let mut ans = 0;
+    let mut i = _input.chars();
+    while let Some(c) = i.next() {
+        match c {
+            '(' => {
+                let mut left = String::new();
+                for c2 in i.by_ref() {
+                    if c2 == 'x' {
+                        break;
+                    } else {
+                        left.push(c2);
+                    }
+                }
+                let mut right = String::new();
+                for c2 in i.by_ref() {
+                    if c2 == ')' {
+                        break;
+                    } else {
+                        right.push(c2);
+                    }
+                }
+                let chars_count = left.parse::<usize>().unwrap();
+                let repeat = right.parse::<usize>().unwrap();
+                for _ in 0..chars_count {
+                    i.next().unwrap();
+                }
+                ans += chars_count * repeat;
+            }
+            _ => ans += 1,
+        }
+    }
+    ans
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     use rstest::*;
 
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
-    }
     #[rstest]
-    fn test_process_1(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process_2(#[case] input: &str, #[case] expected: usize) {
+    #[case("ADVENT", 6)]
+    #[case("A(1x5)BC", 7)]
+    #[case("(3x3)XYZ", 9)]
+    #[case("A(2x2)BCD(2x2)EFG", 11)]
+    #[case("(6x1)(1x3)A", 6)]
+    #[case("X(8x2)(3x3)ABCY", 18)]
+    fn test_process(#[case] input: &str, #[case] expected: usize) {
         assert_eq!(process(input), expected);
     }
 }
