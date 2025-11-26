e2645b30c8952ecde41f53254bca8b885847650d
vuongkienthanh <thanhstardust@outlook.com>
2025-11-26 16:05:53 +0700

    ASD

--- 2016/day21/src/parsing.rs
+++ 2016/day21/src/parsing.rs
@@ -1,18 +1,69 @@
-#[allow(unused_imports)]
-// use aoc_helper::nom::parse_number;
+use aoc_helper::nom::parse_number;
 use nom::{
-    bytes::complete::tag,
-    character::complete::{alpha1, line_ending},
-    multi::separated_list1,
-    sequence::{delimited, preceded, separated_pair, terminated},
     IResult, Parser,
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{anychar, line_ending},
+    multi::separated_list1,
 };
-// https://github.com/rust-bakery/nom/blob/main/doc/choosing_a_combinator.md
 
-type Item = usize;
+#[derive(Debug)]
+pub enum Item {
+    SwapPosition(usize, usize),
+    SwapLetter(char, char),
+    MovePosition(usize, usize),
+    Reverse(usize, usize),
+    RotateRight(usize),
+    RotateLeft(usize),
+    RotateBased(char),
+}
 
 fn parse_line(input: &str) -> IResult<&str, Item> {
-    todo!()
+    alt((
+        (
+            tag("swap position "),
+            parse_number,
+            tag(" with position "),
+            parse_number,
+        )
+            .map(|(_, a, _, b)| Item::SwapPosition(a, b)),
+        (tag("swap letter "), anychar, tag(" with letter "), anychar)
+            .map(|(_, a, _, b)| Item::SwapLetter(a, b)),
+        (
+            tag("move position "),
+            parse_number,
+            tag(" to position "),
+            parse_number,
+        )
+            .map(|(_, a, _, b)| Item::MovePosition(a, b)),
+        (
+            tag("reverse positions "),
+            parse_number,
+            tag(" through "),
+            parse_number,
+        )
+            .map(|(_, a, _, b)| Item::Reverse(a, b)),
+        (
+            tag("rotate right "),
+            parse_number,
+            alt((tag(" steps"), tag(" step"))),
+        )
+            .map(|(_, a, _)| Item::RotateRight(a)),
+        (
+            tag("rotate left "),
+            parse_number,
+            alt((tag(" steps"), tag(" step"))),
+        )
+            .map(|(_, a, _)| Item::RotateLeft(a)),
+        (
+            tag("rotate left "),
+            parse_number,
+            alt((tag(" steps"), tag(" step"))),
+        )
+            .map(|(_, a, _)| Item::RotateLeft(a)),
+        (tag("rotate based on position of letter "), anychar).map(|(_, a)| Item::RotateBased(a)),
+    ))
+    .parse(input)
 }
 
 pub fn parse_input(input: &str) -> IResult<&str, Vec<Item>> {
--- 2016/day21/src/part2.rs
+++ 2016/day21/src/part2.rs
@@ -1,7 +1,69 @@
-use crate::parsing::parse_input;
+use crate::parsing::{Item, parse_input};
+use std::collections::VecDeque;
 
-pub fn process(_input: &str) -> usize {
+pub fn process(_input: &str) -> String {
     let (_, input) = parse_input(_input).unwrap();
+    let mut s: VecDeque<char> = "fbgdceah".chars().collect();
+    for item in input.into_iter().rev() {
+        // println!("{}", s.iter().collect::<String>());
+        // println!("{:?}", item);
+        // println!();
+        match item {
+            Item::SwapPosition(a, b) => s.swap(a, b),
+            Item::SwapLetter(a, b) => {
+                for i in 0..s.len() {
+                    unsafe {
+                        if s.get(i).unwrap_unchecked() == &a {
+                            *s.get_mut(i).unwrap_unchecked() = b;
+                            continue;
+                        }
+                        if s.get(i).unwrap_unchecked() == &b {
+                            *s.get_mut(i).unwrap_unchecked() = a;
+                            continue;
+                        }
+                    }
+                }
+            }
+            Item::MovePosition(a, b) => unsafe {
+                let removed = s.remove(b).unwrap_unchecked();
+                s.insert(a, removed);
+            },
+            Item::Reverse(a, b) => {
+                for i in 0.. {
+                    let left = a + i;
+                    let right = b - i;
+                    if left >= right {
+                        break;
+                    }
+                    s.swap(left, right);
+                }
+            }
+            Item::RotateLeft(a) => s.rotate_right(a),
+            Item::RotateRight(a) => s.rotate_left(a),
+            Item::RotateBased(a) => {
+                for r in 1..s.len() {
+                    let mut maybe = s.clone();
+                    maybe.rotate_left(r);
 
-    todo!("part2")
+                    let mut trying = maybe.clone();
+                    unsafe {
+                        let first_a_index = trying
+                            .iter()
+                            .enumerate()
+                            .find_map(|(i, c)| (c == &a).then_some(i))
+                            .unwrap_unchecked();
+                        trying.rotate_right(1 + first_a_index);
+                        if first_a_index >= 4 {
+                            trying.rotate_right(1);
+                        }
+                    }
+                    if trying == s {
+                        s = maybe;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    s.into_iter().collect()
 }
