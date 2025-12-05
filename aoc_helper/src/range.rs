f315966ee3c226582807f92fade4095c59859156
vuongkienthanh <thanhstardust@outlook.com>
2025-12-06 05:39:04 +0700

    fix aoc_helper::range::merge

--- aoc_helper/src/range.rs
+++ aoc_helper/src/range.rs
@@ -19,32 +19,21 @@ where
     None
 }
 
-pub fn merge<T>(input: Vec<(T, T)>) -> Vec<(T, T)>
+pub fn merge<T>(mut input: Vec<(T, T)>) -> Vec<(T, T)>
 where
     T: PrimInt + One,
 {
     let mut ans = vec![];
-    for i in &input {
-        let mut merged = false;
-        let mut new_ans = vec![];
-        while let Some(j) = ans.pop() {
-            if let Some(k) = merge_two_ranges(*i, j) {
-                merged = true;
-                new_ans.push(k);
-                new_ans.extend(ans);
-                break;
-            } else {
-                new_ans.push(j)
-            }
+    input.sort_unstable_by(|(a, _), (b, _)| b.cmp(a));
+    let mut current = input.pop().unwrap();
+    while let Some(r) = input.pop() {
+        if let Some(merged) = merge_two_ranges(current, r) {
+            current = merged;
+        } else {
+            ans.push(current);
+            current = r;
         }
-        if !merged {
-            new_ans.push(*i);
-        }
-        ans = new_ans;
     }
-    if input.len() == ans.len() {
-        return input;
-    } else {
-        return merge(ans);
-    }
+    ans.push(current);
+    ans
 }
