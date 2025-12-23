8fc28f42931cfee2fe882517a028f204cd6dfcf6
vuongkienthanh <thanhstardust@outlook.com>
2025-12-23 22:13:48 +0700

    2017 day3

--- 2017/day03/src/lib.rs
+++ 2017/day03/src/lib.rs
@@ -4,32 +4,18 @@ use std::iter::repeat_n;
 
 type Point = (isize, isize);
 
-struct Spiral {
-    layer: isize,
-}
-
-impl Spiral {
-    fn next(&mut self) -> Vec<Point> {
-        let ans = Spiral::generate(self.layer);
-        self.layer += 1;
-        ans
-    }
-
-    fn generate(l: isize) -> Vec<Point> {
-        if l == 0 {
-            return vec![(0, 0)];
-        }
-        let side = 2 * l as usize;
-        (-l + 1..=l)
-            .chain(repeat_n(l, side))
-            .chain((-l..=l - 1).rev())
-            .chain(repeat_n(-l, side))
-            .zip(
-                repeat_n(l, side)
-                    .chain((-l..=l - 1).rev())
-                    .chain(repeat_n(-l, side))
-                    .chain(-l + 1..l),
-            )
-            .collect()
-    }
+fn generate(layer: usize) -> Vec<Point> {
+    let l = layer as isize;
+    let side = 2 * layer as usize;
+    (-l + 1..=l)
+        .chain(repeat_n(l, side))
+        .chain((-l..=l - 1).rev())
+        .chain(repeat_n(-l, side))
+        .zip(
+            repeat_n(l, side)
+                .chain((-l..=l - 1).rev())
+                .chain(repeat_n(-l, side))
+                .chain(-l + 1..=l),
+        )
+        .collect()
 }
--- 2017/day03/src/part1.rs
+++ 2017/day03/src/part1.rs
@@ -1,17 +1,18 @@
-use crate::Spiral;
+use crate::generate;
 pub fn process(_input: &str) -> usize {
     let input: usize = _input.parse().unwrap();
-    let mut range = 1..=1usize;
+    let mut end = 1usize;
     for layer in 1.. {
-        range = range.end() + 1..=range.end() + 8 * layer;
+        let range = end + 1..=end + 8 * layer;
         if range.contains(&input) {
             return range
-                .zip(Spiral::generate(layer as isize))
+                .zip(generate(layer))
                 .find_map(|(ele, (p0, p1))| {
-                    (ele == input).then_some(p0.unsigned_abs() + p1.unsigned_abs())
+                    (ele == input).then(|| p0.unsigned_abs() + p1.unsigned_abs())
                 })
                 .unwrap();
         }
+        end = *range.end();
     }
 
     panic!("should have an answer")
