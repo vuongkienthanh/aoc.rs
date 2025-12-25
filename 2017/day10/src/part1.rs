4bce62962790dfb6e96b70f731658d2ef3d6dc49
vuongkienthanh <thanhstardust@outlook.com>
2025-12-26 06:36:51 +0700

    fin day10

--- 2017/day10/src/part1.rs
+++ 2017/day10/src/part1.rs
@@ -2,23 +2,21 @@ use crate::parsing::parse_input;
 
 pub fn process(_input: &str) -> usize {
     let input = parse_input(_input);
-    let mut list: Vec<usize> = (0..256usize).into_iter().collect();
+    let mut list: Vec<usize> = (0..256usize).collect();
     transform(&mut list, input);
     list.into_iter().take(2).product()
 }
 
 fn transform(list: &mut [usize], input: Vec<usize>) {
     let mut i = 0;
-    let mut skip_size = 0;
 
-    for length in input {
+    for (skip_size, length) in input.iter().enumerate() {
         let half = length / 2;
         for left in 0..half {
             let right = length - 1 - left;
             list.swap((i + left) % list.len(), (i + right) % list.len());
         }
         i = (i + length + skip_size) % list.len();
-        skip_size += 1;
     }
 }
 #[cfg(test)]
