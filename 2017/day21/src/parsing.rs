b9a24bbaa57fc329e0d40748a1e995cad6f952a5
vuongkienthanh <thanhstardust@outlook.com>
2025-12-29 19:03:03 +0700

    fin 21

--- 2017/day21/src/parsing.rs
+++ 2017/day21/src/parsing.rs
@@ -1,23 +1,38 @@
 #[allow(unused_imports)]
 // use aoc_helper::nom::parse_signed_usize;
 use nom::{
+    IResult, Parser,
     branch::alt,
     bytes::complete::tag,
     character::complete::{self, alpha1, line_ending},
     combinator::all_consuming,
-    multi::separated_list1,
+    multi::{count, separated_list1},
     sequence::{delimited, preceded, separated_pair, terminated},
-    IResult, Parser,
 };
-// https://github.com/rust-bakery/nom/blob/main/doc/choosing_a_combinator.md
 
-type Item = usize;
+pub type Square = Vec<Vec<char>>;
 
-fn parse_line(input: &str) -> IResult<&str, Item> {
-    todo!()
+fn parse_square_2(input: &str) -> IResult<&str, Square> {
+    separated_list1(tag("/"), count(complete::anychar, 2)).parse(input)
+}
+fn parse_square_3(input: &str) -> IResult<&str, Square> {
+    separated_list1(tag("/"), count(complete::anychar, 3)).parse(input)
+}
+fn parse_square_4(input: &str) -> IResult<&str, Square> {
+    separated_list1(tag("/"), count(complete::anychar, 4)).parse(input)
 }
 
-pub fn parse_input(input: &str) -> Vec<Item> {
+fn parse_line_1(input: &str) -> IResult<&str, (Square, Square)> {
+    separated_pair(parse_square_2, tag(" => "), parse_square_3).parse(input)
+}
+fn parse_line_2(input: &str) -> IResult<&str, (Square, Square)> {
+    separated_pair(parse_square_3, tag(" => "), parse_square_4).parse(input)
+}
+fn parse_line(input: &str) -> IResult<&str, (Square, Square)> {
+    alt((parse_line_1, parse_line_2)).parse(input)
+}
+
+pub fn parse_input(input: &str) -> Vec<(Square, Square)> {
     all_consuming(separated_list1(line_ending, parse_line))
         .parse(input)
         .unwrap()
--- 2017/day21/src/part1.rs
+++ 2017/day21/src/part1.rs
@@ -1,29 +1,54 @@
-use crate::parsing::parse_input;
+use crate::build_map;
+use crate::parsing::{Square, parse_input};
 
 pub fn process(_input: &str) -> usize {
     let input = parse_input(_input);
-    println!("{input:?}");
+    let map = build_map(input);
+    let mut grid: Square = r#".#.
+..#
+###"#
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect();
 
-    todo!("part1")
-    panic!("should have an answer")
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rstest::*;
+    for _ in 0..5 {
+        let size = grid.len();
+        let mut new_grid = vec![];
+        if size.is_multiple_of(2) {
+            for row_chunk in grid.chunks(2) {
+                let mut new_chunk = vec![vec![]; 3];
+                for (a, b) in row_chunk[0].chunks(2).zip(row_chunk[1].chunks(2)) {
+                    let square = vec![a.to_vec(), b.to_vec()];
+                    for (i, row) in map.get(&square).cloned().unwrap().into_iter().enumerate() {
+                        new_chunk[i].extend(row)
+                    }
+                }
+                for row in new_chunk {
+                    new_grid.push(row)
+                }
+            }
+        } else if size.is_multiple_of(3) {
+            for row_chunk in grid.chunks(3) {
+                let mut new_chunk = vec![vec![]; 4];
+                for ((a, b), c) in row_chunk[0]
+                    .chunks(3)
+                    .zip(row_chunk[1].chunks(3))
+                    .zip(row_chunk[2].chunks(3))
+                {
+                    let square = vec![a.to_vec(), b.to_vec(), c.to_vec()];
+                    for (i, row) in map.get(&square).cloned().unwrap().into_iter().enumerate() {
+                        new_chunk[i].extend(row)
+                    }
+                }
+                for row in new_chunk {
+                    new_grid.push(row)
+                }
+            }
+        }
+        grid = new_grid;
+    }
 
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
-    }
-    #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
-    }
+    grid.into_iter()
+        .map(|line| line.into_iter().filter(|x| *x == '#').count())
+        .sum()
 }
