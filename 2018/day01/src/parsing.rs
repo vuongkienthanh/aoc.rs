83bfcba5f3bbcbdcfa47e068b970447c8a876c66
vuongkienthanh <thanhstardust@outlook.com>
2025-12-30 22:31:57 +0700

    2018 day2

--- 2018/day01/src/parsing.rs
+++ 2018/day01/src/parsing.rs
@@ -1,20 +1,14 @@
-#[allow(unused_imports)]
-// use aoc_helper::nom::parse_signed_usize;
 use nom::{
-    branch::alt,
-    bytes::complete::tag,
-    character::complete::{self, alpha1, line_ending},
+    IResult, Parser,
+    character::complete::{self, line_ending},
     combinator::all_consuming,
     multi::separated_list1,
-    sequence::{delimited, preceded, separated_pair, terminated},
-    IResult, Parser,
 };
-// https://github.com/rust-bakery/nom/blob/main/doc/choosing_a_combinator.md
 
-type Item = usize;
+type Item = isize;
 
 fn parse_line(input: &str) -> IResult<&str, Item> {
-    todo!()
+    complete::isize.parse(input)
 }
 
 pub fn parse_input(input: &str) -> Vec<Item> {
