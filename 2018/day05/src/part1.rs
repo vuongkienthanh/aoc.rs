64e32030b76d97d380960d5227581f72b2061bf8
vuongkienthanh <thanhstardust@outlook.com>
2025-12-31 15:56:45 +0700

    day 5

--- 2018/day05/src/part1.rs
+++ 2018/day05/src/part1.rs
@@ -1,29 +1,42 @@
-use crate::parsing::parse_input;
+use std::collections::VecDeque;
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
+    react(_input).len()
+}
 
-    todo!("part1");
-    // panic!("should have an answer")
+fn react(input: &str) -> VecDeque<u8> {
+    let mut left: VecDeque<u8> = VecDeque::new();
+    let mut right: VecDeque<u8> = VecDeque::new();
+    right.extend(input.bytes());
+
+    while let Some(r) = right.pop_front() {
+        if let Some(l) = left.pop_back() {
+            if l.abs_diff(r) != 32 {
+                left.push_back(l);
+                left.push_back(r);
+            }
+        } else {
+            left.push_back(r);
+        }
+    }
+
+    left
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     use rstest::*;
 
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
-    }
     #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
+    #[case("dabAcCaCBAcCcaDA", "dabCBAcaDA")]
+    #[case("aA", "")]
+    #[case("abBA", "")]
+    #[case("abAB", "abAB")]
+    #[case("aabAAB", "aabAAB")]
+    fn test_react(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(
+            react(input).into_iter().map(|x| x as char).collect::<String>(),
+            expected.to_string()
+        );
     }
 }
--- 2018/day05/src/part2.rs
+++ 2018/day05/src/part2.rs
@@ -1,6 +1,27 @@
-use crate::parsing::parse_input;
+use std::collections::VecDeque;
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    todo!("part2");
+    ('a'..='z').map(|c| react(_input, c).len()).min().unwrap()
+}
+fn react(input: &str, rm: char) -> VecDeque<u8> {
+    let mut left: VecDeque<u8> = VecDeque::new();
+    let mut right: VecDeque<u8> = VecDeque::new();
+    right.extend(input.bytes());
+    let (rm0, rm1) = (rm as u8, rm as u8 - 32);
+
+    while let Some(r) = right.pop_front() {
+        if r == rm0 || r == rm1 {
+            continue;
+        }
+        if let Some(l) = left.pop_back() {
+            if l.abs_diff(r) != 32 {
+                left.push_back(l);
+                left.push_back(r);
+            }
+        } else {
+            left.push_back(r);
+        }
+    }
+
+    left
 }
