6971a2949ca25a09c461b45153021d9130d55e92
vuongkienthanh <thanhstardust@outlook.com>
2026-01-03 06:35:33 +0700

    ASD

--- 2018/day12/src/parsing.rs
+++ 2018/day12/src/parsing.rs
@@ -1,25 +1,67 @@
-#[allow(unused_imports)]
-// use aoc_helper::nom::parse_signed_usize;
+use fxhash::FxHashMap;
 use nom::{
+    IResult, Parser,
     branch::alt,
     bytes::complete::tag,
-    character::complete::{self, alpha1, line_ending},
+    character::complete::{self, line_ending},
     combinator::all_consuming,
-    multi::separated_list1,
-    sequence::{delimited, preceded, separated_pair, terminated},
-    IResult, Parser,
+    multi::{count, many1, separated_list1},
+    sequence::{preceded, separated_pair},
 };
-// https://github.com/rust-bakery/nom/blob/main/doc/choosing_a_combinator.md
+use std::collections::VecDeque;
 
-type Item = usize;
+pub type Map = FxHashMap<Vec<usize>, isize>;
 
-fn parse_line(input: &str) -> IResult<&str, Item> {
-    todo!()
+fn parse_plants(input: &str) -> IResult<&str, VecDeque<usize>> {
+    preceded(
+        tag("initial state: "),
+        many1(alt((
+        complete::char('.').map(|_| 0),
+        complete::char('#').map(|_| 1),
+    ))))
+    .map(|mut v| {
+        v[0] += 2;
+        v.into_iter().collect()
+    })
+    .parse(input)
 }
 
-pub fn parse_input(input: &str) -> Vec<Item> {
-    all_consuming(separated_list1(line_ending, parse_line))
+fn parse_map_line(input: &str) -> IResult<&str, Option<(Vec<usize>, isize)>> {
+    separated_pair(
+        count(alt((complete::char('.'), complete::char('#'))), 5),
+        tag(" => "),
+        alt((complete::char('.'), complete::char('#'))),
+    )
+    .map(|(a, b)| {
+        if a[2] == b {
+            None
+        } else {
+            Some((
+                a.iter()
+                    .enumerate()
+                    .take(2)
+                    .chain(a.iter().enumerate().skip(3))
+                    .filter_map(|(i, x)| (x == &'.').then_some(i))
+                    .collect(),
+                if b == '.' { -1 } else { 1 },
+            ))
+        }
+    })
+    .parse(input)
+}
+fn parse_map(input: &str) -> IResult<&str, Map> {
+    separated_list1(line_ending, parse_map_line)
+        .map(|v| v.into_iter().flatten().collect())
         .parse(input)
-        .unwrap()
-        .1
+}
+
+pub fn parse_input(input: &str) -> (VecDeque<usize>, Map) {
+    all_consuming(separated_pair(
+        parse_plants,
+        (line_ending, line_ending),
+        parse_map,
+    ))
+    .parse(input)
+    .unwrap()
+    .1
 }
--- 2018/day12/src/part1.rs
+++ 2018/day12/src/part1.rs
@@ -1,12 +1,59 @@
 use crate::parsing::parse_input;
+use std::collections::VecDeque;
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
+    let (mut plants, map) = parse_input(_input);
+    println!("{plants:?}");
 
-    todo!("part1");
-    // panic!("should have an answer")
+    for _ in 0..1 {
+        // for _ in 0..20 {
+        add_padding(&mut plants);
+        let mut new_plants = VecDeque::new();
+        let mut windows: VecDeque<usize> = (0..5).map(|_| plants.pop_front().unwrap()).collect();
+        while let Some(p) = plants.pop_front() {
+            windows.pop_front();
+            windows.push_back(p);
+            let no_plants_loc: Vec<usize> = windows
+                .iter()
+                .enumerate()
+                .filter_map(|(i, x)| (x % 2 == 0).then_some(i))
+                .collect();
+            let current = windows[2];
+            if let Some(i) = map.get(&no_plants_loc) {
+                new_plants.push_back(current.strict_add_signed(*i));
+            } else {
+                new_plants.push_back(current);
+            }
+        }
+        plants = new_plants;
+    }
+    println!("{plants:?}");
+
+    0
 }
+
+fn add_padding(plants: &mut VecDeque<usize>) {
+    let first_plant_loc = plants
+        .iter()
+        .enumerate()
+        .find_map(|(i, x)| ((x % 2) == 1).then_some(i))
+        .unwrap();
+    for _ in 0..3usize.saturating_sub(first_plant_loc) {
+        plants.push_front(0);
+    }
+    let last_plant_loc = plants
+        .iter()
+        .rev()
+        .enumerate()
+        .find_map(|(i, x)| ((x % 2) == 1).then_some(i))
+        .unwrap();
+    for _ in 0..3usize.saturating_sub(last_plant_loc) {
+        plants.push_back(0);
+    }
+}
+// fn remove_padding(plants: &mut VecDeque<usize>) {
+// }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,16 +61,25 @@ mod tests {
 
     #[fixture]
     pub fn fixture() -> &'static str {
-        r#""#
+        r#"initial state: #..#.#..##......###...###
+
+...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #"#
     }
     #[rstest]
     fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
+        assert_eq!(process(fixture), 325);
     }
 }
