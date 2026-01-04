1bf2712c20a30247c3456ba726f483f972942ea9
vuongkienthanh <54462611+vuongkienthanh@users.noreply.github.com>
2026-01-04 08:51:01 +0700

    fin day 14

--- 2018/day14/src/part1.rs
+++ 2018/day14/src/part1.rs
@@ -1,29 +1,24 @@
-use crate::parsing::parse_input;
-
-pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
-
-    todo!("part1");
-    // panic!("should have an answer")
+pub fn process(_input: &str) -> String {
+    let mut v = vec![3u8, 7];
+    let (mut a, mut b) = (0, 1);
+    
+    let input = _input.parse::<usize>().unwrap();
+    
+    for _ in 0..input + 10 {
+        step(&mut v, &mut a, &mut b);
+    }
+    v.into_iter().skip(input).take(10).map(|x| char::from_digit(x as u32, 10).unwrap()).collect::<String>()
 }
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rstest::*;
 
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
+fn step(v: &mut Vec<u8>, a: &mut usize, b: &mut usize) {
+    // println!("{v:?}");
+    let n = v[*a] + v[*b];
+    if n >= 10 {
+        v.push(1);
+        v.push(n%10);
+    } else {
+        v.push(n);
     }
-    #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
-    }
+    *a = (*a + 1 + v[*a] as usize) % v.len();
+    *b = (*b + 1 + v[*b] as usize) % v.len();
 }
