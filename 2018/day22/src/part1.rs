0d7782933b998153c18be18fdc6b00ce8a32b3cd
vuongkienthanh <thanhstardust@outlook.com>
2026-01-14 23:48:34 +0700

    fin 23

--- 2018/day22/src/part1.rs
+++ 2018/day22/src/part1.rs
@@ -1,24 +1,15 @@
 use crate::parsing::parse_input;
+use crate::{Cache, get_type};
 
 pub fn process(_input: &str) -> usize {
     let (depth, target) = parse_input(_input);
-    let mut ero1: Vec<usize> = vec![];
+    let mut cache = Cache::new();
     let mut ans = 0;
 
     for y in 0..target.1 + 1 {
-        let mut ero2 = vec![];
         for x in 0..target.0 + 1 {
-            let geo = match (x, y) {
-                (0, 0) => 0,
-                loc if loc == target => 0,
-                (x, 0) => x * 16807,
-                (0, y) => y * 48271,
-                (x, _) => ero2.last().unwrap() * ero1[x],
-            };
-            ero2.push((geo + depth) % 20183);
-            ans += ero2.last().unwrap() % 3;
+            ans += get_type((x, y), depth, target, &mut cache);
         }
-        ero1 = ero2;
     }
     ans
 }
