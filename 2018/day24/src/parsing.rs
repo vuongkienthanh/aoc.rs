7a46eadf18eff99eaaa7551318360810b8b2b1b8
vuongkienthanh <thanhstardust@outlook.com>
2026-01-19 06:43:33 +0700

    ASD

--- 2018/day24/src/parsing.rs
+++ 2018/day24/src/parsing.rs
@@ -1,25 +1,163 @@
-#[allow(unused_imports)]
-// use aoc_helper::nom::parse_signed_usize;
 use nom::{
+    IResult, Parser,
     branch::alt,
-    bytes::complete::tag,
-    character::complete::{self, alpha1, line_ending},
+    bytes::complete::{tag, take},
+    character::complete::{self, line_ending},
     combinator::all_consuming,
     multi::separated_list1,
-    sequence::{delimited, preceded, separated_pair, terminated},
-    IResult, Parser,
+    sequence::{delimited, preceded, separated_pair},
 };
-// https://github.com/rust-bakery/nom/blob/main/doc/choosing_a_combinator.md
 
-type Item = usize;
-
-fn parse_line(input: &str) -> IResult<&str, Item> {
-    todo!()
+#[derive(Debug, Eq, PartialEq)]
+pub enum DamageType {
+    Slashing,
+    Radiation,
+    Fire,
+    Bludgeoning,
+    Cold,
 }
 
-pub fn parse_input(input: &str) -> Vec<Item> {
-    all_consuming(separated_list1(line_ending, parse_line))
+#[derive(Debug)]
+pub struct Item {
+    pub units: usize,
+    pub hp: usize,
+    pub weakness: Vec<DamageType>,
+    pub immune: Vec<DamageType>,
+    pub atk: usize,
+    pub atk_type: DamageType,
+    pub initiative: usize,
+}
+
+#[derive(Debug)]
+pub enum Team {
+    Immune,
+    Infection,
+}
+impl Team {
+    pub fn is_enemy(a: &Group, b: &Group) -> bool {
+        matches!(
+            (&a.team, &b.team),
+            (Team::Immune, Team::Infection) | (Team::Infection, Team::Immune),
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct Group {
+    pub team: Team,
+    pub is_alive: bool,
+    pub stats: Item,
+}
+impl Group {
+    pub fn effective_power(&self) -> usize {
+        self.stats.units * self.stats.atk
+    }
+}
+
+fn parse_damage_type(input: &str) -> IResult<&str, DamageType> {
+    alt((
+        tag("fire").map(|_| DamageType::Fire),
+        tag("cold").map(|_| DamageType::Cold),
+        tag("radiation").map(|_| DamageType::Radiation),
+        tag("slashing").map(|_| DamageType::Slashing),
+        tag("bludgeoning").map(|_| DamageType::Bludgeoning),
+    ))
+    .parse(input)
+}
+fn parse_damage_types(input: &str) -> IResult<&str, Vec<DamageType>> {
+    separated_list1(tag(", "), parse_damage_type).parse(input)
+}
+
+fn parse_immune(input: &str) -> IResult<&str, Vec<DamageType>> {
+    preceded(tag("immune to "), parse_damage_types).parse(input)
+}
+fn parse_weakness(input: &str) -> IResult<&str, Vec<DamageType>> {
+    preceded(tag("weak to "), parse_damage_types).parse(input)
+}
+fn parse_weakness_immune(input: &str) -> IResult<&str, (Vec<DamageType>, Vec<DamageType>)> {
+    alt((
+        delimited(
+            tag("("),
+            separated_pair(parse_weakness, tag("; "), parse_immune),
+            tag(") "),
+        ),
+        delimited(
+            tag("("),
+            separated_pair(parse_immune, tag("; "), parse_weakness).map(|(a, b)| (b, a)),
+            tag(") "),
+        ),
+        delimited(tag("("), parse_weakness.map(|w| (w, vec![])), tag(") ")),
+        delimited(tag("("), parse_immune.map(|i| (vec![], i)), tag(") ")),
+        take(0usize).map(|_| (vec![], vec![])),
+    ))
+    .parse(input)
+}
+
+fn parse_item(input: &str) -> IResult<&str, Item> {
+    ((
+        complete::usize,
+        tag(" units each with "),
+        complete::usize,
+        tag(" hit points "),
+        parse_weakness_immune,
+        tag("with an attack that does "),
+        complete::usize,
+        tag(" "),
+        parse_damage_type,
+        tag(" damage at initiative "),
+        complete::usize,
+    ))
+        .map(
+            |(units, _, hp, _, (weakness, immune), _, atk, _, atk_type, _, initiative)| Item {
+                units,
+                hp,
+                weakness,
+                immune,
+                atk,
+                atk_type,
+                initiative,
+            },
+        )
         .parse(input)
-        .unwrap()
-        .1
+}
+
+fn parse_immune_system(input: &str) -> IResult<&str, Vec<Group>> {
+    preceded(
+        (tag("Immune System:"), line_ending),
+        separated_list1(
+            line_ending,
+            parse_item.map(|item| Group {
+                team: Team::Immune,
+                stats: item,
+            }),
+        ),
+    )
+    .parse(input)
+}
+
+fn parse_infection(input: &str) -> IResult<&str, Vec<Group>> {
+    preceded(
+        (tag("Infection:"), line_ending),
+        separated_list1(
+            line_ending,
+            parse_item.map(|item| Group {
+                team: Team::Infection,
+                stats: item,
+            }),
+        ),
+    )
+    .parse(input)
+}
+
+pub fn parse_input(input: &str) -> Vec<Group> {
+    let (mut team1, team2) = all_consuming(separated_pair(
+        parse_immune_system,
+        (line_ending, line_ending),
+        parse_infection,
+    ))
+    .parse(input)
+    .unwrap()
+    .1;
+    team1.extend(team2);
+    team1
 }
--- 2018/day24/src/part1.rs
+++ 2018/day24/src/part1.rs
@@ -1,12 +1,90 @@
-use crate::parsing::parse_input;
+use crate::parsing::{DamageType, Group, Team, parse_input};
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
+    let mut groups = parse_input(_input);
 
-    todo!("part1");
-    // panic!("should have an answer")
+    loop {
+        let target_plan = target_selection(&mut groups);
+        attacking(&mut groups, target_plan);
+        if finish(&groups) {
+            break;
+        }
+    }
+    groups.into_iter().map(|g| g.stats.units).sum()
 }
+
+fn target_selection(groups: &mut Vec<Group>) -> Vec<Option<usize>> {
+    groups.sort_unstable_by(|a, b| {
+        (b.effective_power(), b.stats.initiative).cmp(&(a.effective_power(), a.stats.initiative))
+    });
+    let mut ans = vec![];
+    let mut chosen_def_groups: Vec<usize> = vec![];
+    for a in groups.iter() {
+        let mut max_damage = usize::MIN;
+        let mut chosen = None;
+        for (i, b) in groups
+            .iter()
+            .enumerate()
+            .filter(|(i, b)| Team::is_enemy(a, b) && !chosen_def_groups.contains(&i))
+        {
+            let dealt = deal_damage(a, b);
+            if dealt > max_damage {
+                max_damage = dealt;
+                chosen = Some(i);
+            }
+        }
+        if let Some(i) = chosen.clone() {
+            chosen_def_groups.push(i);
+        }
+        ans.push(chosen);
+    }
+
+    ans
+}
+
+fn attacking(groups: &mut Vec<Group>, plan: Vec<Option<usize>>) {
+    let mut orders: Vec<(usize, Option<usize>)> = plan.into_iter().enumerate().collect();
+    let mut dead_list = vec![];
+
+    orders.sort_unstable_by(|(i, a), (j, b)| {
+        groups
+            .get(*j)
+            .unwrap()
+            .stats
+            .initiative
+            .cmp(&groups.get(*i).unwrap().stats.initiative)
+    });
+    for (i, p) in orders {
+        if let Some(j) = p {
+            let a = groups.get(i).unwrap();
+            let b = groups.get(j).unwrap();
+            let dealt = deal_damage(a, b);
+            let killed = dealt / b.stats.hp;
+            match b.stats.units.checked_sub(killed) {
+                None | Some(0) => {}
+                Some(x) => {
+                    groups.get_mut(j).unwrap().stats.units = x;
+                }
+            }
+        }
+    }
+}
+
+fn finish(groups: &[Group]) -> bool {
+    groups.iter().all(|g| matches!(g.team, Team::Immune))
+        || groups.iter().all(|g| matches!(g.team, Team::Infection))
+}
+
+fn deal_damage(a: &Group, b: &Group) -> usize {
+    if b.stats.immune.contains(&a.stats.atk_type) {
+        0
+    } else if b.stats.weakness.contains(&a.stats.atk_type) {
+        a.effective_power() * 2
+    } else {
+        a.effective_power()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,16 +92,16 @@ mod tests {
 
     #[fixture]
     pub fn fixture() -> &'static str {
-        r#""#
+        r#"Immune System:
+17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
+989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
+
+Infection:
+801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
+4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4"#
     }
     #[rstest]
     fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
+        assert_eq!(process(fixture), 5216);
     }
 }
