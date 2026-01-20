bbd981c66f36e6368edf5a4da38d4ce451c44855
vuongkienthanh <thanhstardust@outlook.com>
2026-01-20 22:34:17 +0700

    ASD

--- 2019/day10/src/part1.rs
+++ 2019/day10/src/part1.rs
@@ -1,28 +1,105 @@
-use crate::parsing::parse_input;
+use crate::parsing::parse;
+use fraction::GenericFraction;
+use std::collections::BTreeSet;
+type F = GenericFraction<usize>;
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
-
-    todo!("part1");
-    // panic!("should have an answer")
+    let input = parse(_input);
+    find_best_location(&input).0
 }
+
+pub fn find_best_location(input: &[(usize, usize)]) -> (usize, (usize, usize)) {
+    let mut ans = (0, (0, 0));
+    for (x0, y0) in input {
+        let mut count = BTreeSet::new();
+        for (x1, y1) in input {
+            if (x0, y0) == (x1, y1) {
+                continue;
+            }
+            let (dx, dy) = (x0.abs_diff(*x1), y0.abs_diff(*y1));
+            count.insert((F::new(dx, dy), x0 >= x1, y0 >= y1));
+        }
+        ans = ans.max((count.len(), (*x0, *y0)));
+    }
+    ans
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rstest::*;
 
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
-    }
     #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
+    #[case(
+        r#".#..#
+.....
+#####
+....#
+...##"#,
+        8
+    )]
+    #[case(
+        r#"......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####"#,
+        33
+    )]
+    #[case(
+        r#"#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###."#,
+        35
+    )]
+    #[case(
+        r#".#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#.."#,
+        41
+    )]
+    #[case(
+        r#".#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##"#,
+        210
+    )]
     fn test_process(#[case] input: &str, #[case] expected: usize) {
         assert_eq!(process(input), expected);
     }
