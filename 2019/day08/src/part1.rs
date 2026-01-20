c1a3ec90371073dc5edaf7248a5fb0fb4f87cf01
vuongkienthanh <thanhstardust@outlook.com>
2026-01-20 16:55:13 +0700

    fin day 8

--- 2019/day08/src/part1.rs
+++ 2019/day08/src/part1.rs
@@ -2,28 +2,19 @@ use crate::parsing::parse_input;
 
 pub fn process(_input: &str) -> usize {
     let input = parse_input(_input);
-    println!("{input:?}");
 
-    todo!("part1");
-    // panic!("should have an answer")
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rstest::*;
-
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
+    let mut ans = (usize::MAX, 0);
+    for chunk in input.chunks(150) {
+        let (mut zeros, mut ones, mut twos) = (0, 0, 0);
+        for i in chunk {
+            match i {
+                0 => zeros += 1,
+                1 => ones += 1,
+                2 => twos += 1,
+                _ => panic!("should be 0 1 2"),
+            }
+        }
+        ans = ans.min((zeros, ones * twos));
     }
-    #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
-    }
+    ans.1
 }
