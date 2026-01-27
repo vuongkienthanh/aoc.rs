e8c20468f9d44d8d38be777623fa0ecdcb08da7e
vuongkienthanh <thanhstardust@outlook.com>
2026-01-27 16:00:38 +0700

    ASD

--- 2019/day19/src/part1.rs
+++ 2019/day19/src/part1.rs
@@ -1,29 +1,25 @@
-use crate::parsing::parse_input;
+use intcode::{Computer, parse};
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
+    let input = parse(_input);
+    let drone = Computer::new(input);
 
-    todo!("part1");
-    // panic!("should have an answer")
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rstest::*;
-
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
+    let mut ans = 0;
+    for y in 0..50 {
+        print!("row = {y}");
+        for x in 0..50 {
+            let mut drone = drone.clone();
+            drone.input(x);
+            drone.input(y);
+            let o = drone.long_run().output() as usize;
+            if o == 0 {
+                print!(".");
+            } else {
+                print!("#");
+            }
+            ans += o;
+        }
+        println!("");
     }
-    #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
-    }
+    ans
 }
