80df78575c4cdccb563176ee8441f728dc1886c9
vuongkienthanh <thanhstardust@outlook.com>
2026-01-30 00:31:33 +0700

    ASD

--- 2019/day20/src/part2.rs
+++ 2019/day20/src/part2.rs
@@ -1,85 +1,62 @@
 use crate::parsing::{Cell, parse};
 use aoc_helper::adj::grid::adj4;
-use std::collections::BTreeMap;
+use fxhash::FxHashSet as Set;
 
 pub fn process(_input: &str) -> usize {
     let (map, portals, start, end) = parse(_input);
     let rows = map.len();
     let cols = map[0].len();
-    let mut seen: Vec<Vec<Vec<usize>>> = vec![vec![vec![]; cols]; rows];
+    let mut seen: Vec<Vec<Set<usize>>> = vec![vec![Set::default(); cols]; rows];
 
-    let mut current = vec![(start, 0, 0, false, BTreeMap::new())];
+    let mut current = vec![(start, 0, 0, false)];
     'a: loop {
         let mut new = vec![];
 
-        for ((r, c), layer, step, teleported, mut walked) in current {
-            if (r, c) == end && layer == 0 {
-                break 'a step;
-            }
-            if layer > 0 && ((r, c) == start || (r, c) == end) {
-                continue;
-            }
+        for ((r, c), layer, step, teleported) in current {
             match map[r][c] {
-                Cell::Wall => panic!("should not be wall"),
+                Cell::Wall => (),
+                Cell::Space if layer == 0 && (r, c) == end => {
+                    break 'a step;
+                }
                 Cell::Space => {
-                    for p2 in adj4((r, c), rows, cols)
-                        .into_iter()
-                        .flatten()
-                        .filter(|(r, c)| {
-                            !matches!(map[*r][*c], Cell::Wall) && !seen[*r][*c].contains(&layer)
-                        })
-                    {
-                        new.push((p2, layer, step + 1, teleported, walked.clone()));
+                    if seen[r][c].insert(layer) {
+                        for p2 in adj4((r, c), rows, cols).into_iter().flatten() {
+                            new.push((p2, layer, step + 1, teleported));
+                        }
                     }
-                    seen[r][c].push(layer);
                 }
                 Cell::InnerPortal(_) | Cell::OuterPortal(_) if teleported => {
-                    for p2 in adj4((r, c), rows, cols)
-                        .into_iter()
-                        .flatten()
-                        .filter(|(r, c)| {
-                            !matches!(map[*r][*c], Cell::Wall) && !seen[*r][*c].contains(&layer)
-                        })
-                    {
-                        new.push((p2, layer, step + 1, false, walked.clone()));
+                    if seen[r][c].insert(layer) {
+                        for p2 in adj4((r, c), rows, cols).into_iter().flatten() {
+                            new.push((p2, layer, step + 1, false));
+                        }
                     }
-                    seen[r][c].push(layer);
                 }
                 Cell::InnerPortal(pt) => {
-                    if let Some(min_layer) = walked.get(&pt)
-                        && layer >= *min_layer
-                    {
-                        continue;
+                    if seen[r][c].insert(layer) {
+                        let p2 = portals
+                            .get(&pt)
+                            .unwrap()
+                            .iter()
+                            .find(|p| p != &&(r, c))
+                            .cloned()
+                            .unwrap();
+                        new.push((p2, layer + 1, step + 1, true));
                     }
-                    walked.insert(pt, layer);
-                    let (r2, c2) = portals
-                        .get(&pt)
-                        .unwrap()
-                        .iter()
-                        .find(|p| p != &&(r, c))
-                        .cloned()
-                        .unwrap();
-                    if seen[r2][c2].contains(&(layer + 1)) {
-                        continue;
-                    }
-                    new.push(((r2, c2), layer + 1, step + 1, true, walked));
-                    seen[r][c].push(layer);
                 }
                 Cell::OuterPortal(pt) if layer > 0 => {
-                    let (r2, c2) = portals
-                        .get(&pt)
-                        .unwrap()
-                        .iter()
-                        .find(|p| p != &&(r, c))
-                        .cloned()
-                        .unwrap();
-                    if seen[r2][c2].contains(&(layer - 1)) {
-                        continue;
+                    if seen[r][c].insert(layer) {
+                        let p2 = portals
+                            .get(&pt)
+                            .unwrap()
+                            .iter()
+                            .find(|p| p != &&(r, c))
+                            .cloned()
+                            .unwrap();
+                        new.push((p2, layer - 1, step + 1, true));
                     }
-                    new.push(((r2, c2), layer - 1, step + 1, true, walked));
-                    seen[r][c].push(layer);
                 }
-                Cell::OuterPortal(_) => continue,
+                Cell::OuterPortal(_) => (),
             }
             // if let Cell::InnerPortal(p) | Cell::OuterPortal(p) = map[r][c] {
             //     println!("step={step}, at {p:?} layer {layer}");
