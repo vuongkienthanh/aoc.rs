2d15e07ce92c321d05c7829d0bea67dd5058b168
vuongkienthanh <thanhstardust@outlook.com>
2026-01-30 11:15:50 +0700

    ASD

--- 2019/day22/src/parsing.rs
+++ 2019/day22/src/parsing.rs
@@ -1,20 +1,28 @@
-#[allow(unused_imports)]
-// use aoc_helper::nom::parse_signed_usize;
+use aoc_helper::nom::{Sign, parse_signed_usize};
 use nom::{
+    IResult, Parser,
     branch::alt,
     bytes::complete::tag,
-    character::complete::{self, alpha1, line_ending},
+    character::complete::{self, line_ending},
     combinator::all_consuming,
     multi::separated_list1,
-    sequence::{delimited, preceded, separated_pair, terminated},
-    IResult, Parser,
+    sequence::preceded,
 };
-// https://github.com/rust-bakery/nom/blob/main/doc/choosing_a_combinator.md
 
-type Item = usize;
+#[derive(Debug)]
+pub enum Item {
+    Reverse,
+    Cut(Sign, usize),
+    Deal(usize),
+}
 
 fn parse_line(input: &str) -> IResult<&str, Item> {
-    todo!()
+    alt((
+        tag("deal into new stack").map(|_| Item::Reverse),
+        preceded(tag("cut "), parse_signed_usize).map(|(s, u)| Item::Cut(s, u)),
+        preceded(tag("deal with increment "), complete::usize).map(Item::Deal),
+    ))
+    .parse(input)
 }
 
 pub fn parse_input(input: &str) -> Vec<Item> {
--- 2019/day22/src/part1.rs
+++ 2019/day22/src/part1.rs
@@ -1,29 +1,93 @@
-use crate::parsing::parse_input;
+use crate::parsing::{Item, parse_input};
+use aoc_helper::nom::Sign;
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
-
-    todo!("part1");
-    // panic!("should have an answer")
+    shuffle(10007, _input)
+        .into_iter()
+        .enumerate()
+        .find_map(|(i, x)| (x == 2019).then_some(i))
+        .unwrap()
 }
+
+fn shuffle(ncards: usize, input: &str) -> Vec<usize> {
+    let input = parse_input(input);
+    let mut stack: Vec<usize> = (0..ncards).collect();
+
+    for item in input {
+        match item {
+            Item::Reverse => stack.reverse(),
+            Item::Cut(s, u) => match s {
+                Sign::Positive => {
+                    let mut new_stack = stack[u..].to_vec();
+                    new_stack.extend(&stack[..u]);
+                    stack = new_stack;
+                }
+                Sign::Negative => {
+                    let u = ncards - u;
+                    let mut new_stack = stack[u..].to_vec();
+                    new_stack.extend(&stack[..u]);
+                    stack = new_stack;
+                }
+            },
+            Item::Deal(x) => {
+                let mut new_stack = vec![0; ncards];
+                let mut pos = 0;
+                for card in stack {
+                    new_stack[pos % ncards] = card;
+                    pos += x;
+                }
+                stack = new_stack;
+            }
+        }
+    }
+
+    stack
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rstest::*;
 
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
-    }
     #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
+    #[case(
+        r#"deal with increment 7
+deal into new stack
+deal into new stack"#,
+        "0 3 6 9 2 5 8 1 4 7"
+    )]
+    #[case(
+        r#"cut 6
+deal with increment 7
+deal into new stack"#,
+        "3 0 7 4 1 8 5 2 9 6"
+    )]
+    #[case(
+        r#"deal with increment 7
+deal with increment 9
+cut -2"#,
+        "6 3 0 7 4 1 8 5 2 9"
+    )]
+    #[case(
+        r#"deal into new stack
+cut -2
+deal with increment 7
+cut 8
+cut -4
+deal with increment 7
+cut 3
+deal with increment 9
+deal with increment 3
+cut -1"#,
+        "9 2 5 8 1 4 7 0 3 6"
+    )]
+    fn test_process(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(
+            shuffle(10, input),
+            expected
+                .split_ascii_whitespace()
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect::<Vec<_>>()
+        );
     }
 }
