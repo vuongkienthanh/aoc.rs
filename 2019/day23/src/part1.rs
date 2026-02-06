039eb659ea2daa5b95cee6e32097131350a45109
vuongkienthanh <thanhstardust@outlook.com>
2026-02-06 11:35:21 +0700

    ASD

--- 2019/day23/src/part1.rs
+++ 2019/day23/src/part1.rs
@@ -1,29 +1,38 @@
-use crate::parsing::parse_input;
+use intcode::{Computer, RunResult, parse};
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
+    let input = parse(_input);
 
-    todo!("part1");
-    // panic!("should have an answer")
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rstest::*;
-
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
-    }
-    #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
+    let mut computers = vec![];
+    for i in 0..50 {
+        let mut comp = Computer::new(input.clone());
+        comp.input(i as i64);
+        computers.push(comp);
     }
 
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
+    'a: loop {
+        for i in 0..50 {
+            let comp = computers.get_mut(i).unwrap();
+            let mut sent = None;
+            match comp.long_run() {
+                RunResult::Output(address) => {
+                    let x = comp.long_run().output();
+                    let y = comp.long_run().output();
+                    if address == 255 {
+                        break 'a y as usize;
+                    }
+                    sent = Some((address, x, y));
+                }
+                RunResult::WaitingInput => {
+                    comp.input(-1);
+                }
+                RunResult::Halt => (),
+            }
+            if let Some((address, x, y)) = sent {
+                let comp = computers.get_mut(address as usize).unwrap();
+                comp.input(x);
+                comp.input(y);
+            }
+        }
     }
 }
