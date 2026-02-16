bf34c36e27134ae98c18c38ba7d6803c91f19eef
vuongkienthanh <thanhstardust@outlook.com>
2026-02-16 20:53:50 +0700

    ASD

--- 2021/day15/src/part1.rs
+++ 2021/day15/src/part1.rs
@@ -1,29 +1,39 @@
-use crate::parsing::parse_input;
+use aoc_helper::adj::grid::adj4;
+use grid::Grid;
 
-pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
+pub fn process(_input: &str) -> u32 {
+    let input = Grid::from(
+        _input
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|x| x.to_digit(10).unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>(),
+    );
+    let rows = input.rows();
+    let cols = input.cols();
+    let start = (0, 0);
+    let end = (input.rows() - 1, input.cols() - 1);
+    let mut risk: Grid<u32> = Grid::new(rows, cols);
+    risk.fill(u32::MAX);
+    risk[start] = 0;
 
-    todo!("part1");
-    // panic!("should have an answer")
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rstest::*;
-
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
+    let mut current = vec![start];
+    while !current.is_empty() {
+        let mut new = vec![];
+        for p in current {
+            for adj in adj4(p, input.rows(), input.cols()).into_iter().flatten() {
+                let new_risk = risk[p] + input[adj];
+                if new_risk < risk[adj] {
+                    risk[adj] = new_risk;
+                    new.push(adj);
+                }
+            }
+        }
+        current = new;
     }
-    #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
 
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
-    }
+    risk[end]
 }
--- 2021/day15/src/part2.rs
+++ 2021/day15/src/part2.rs
@@ -1,6 +1,43 @@
-use crate::parsing::parse_input;
+use aoc_helper::adj::grid::adj4;
+use grid::Grid;
 
-pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    todo!("part2");
+pub fn process(_input: &str) -> u32 {
+    let input = Grid::from(
+        _input
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|x| x.to_digit(10).unwrap() - 1)
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>(),
+    );
+    let rows = input.rows() * 5;
+    let cols = input.cols() * 5;
+    let start = (0, 0);
+    let end = (rows - 1, cols - 1);
+    let mut risk: Grid<u32> = Grid::new(rows, cols);
+    risk.fill(u32::MAX);
+    risk[start] = 0;
+
+    let mut current = vec![start];
+    while !current.is_empty() {
+        let mut new = vec![];
+        for p in current {
+            for adj in adj4(p, rows, cols).into_iter().flatten() {
+                let risk_at_adj = (input[(adj.0 % input.rows(), adj.1 % input.cols())]
+                    + (adj.0 / input.rows()) as u32
+                    + (adj.1 / input.cols()) as u32)
+                    % 9;
+                let new_risk = risk[p] + risk_at_adj + 1;
+                if new_risk < risk[adj] {
+                    risk[adj] = new_risk;
+                    new.push(adj);
+                }
+            }
+        }
+        current = new;
+    }
+
+    risk[end]
 }
