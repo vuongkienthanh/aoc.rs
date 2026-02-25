4d6ba59a329c91f8b25a319a72ea6529bd3b31da
vuongkienthanh <thanhstardust@outlook.com>
2026-02-25 15:29:36 +0700

    ASD

--- 2021/day23/src/part1.rs
+++ 2021/day23/src/part1.rs
@@ -1,11 +1,40 @@
-use crate::parsing::parse_input;
+use crate::{State, available_paths, ignore_rooms, score};
+use fxhash::FxHashMap;
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
+    let mut current = vec![State::from(_input)];
+    let mut seen: FxHashMap<[u8; 8], usize> = FxHashMap::default();
+    seen.insert(current.first().unwrap().locations.clone(), 0);
+    let mut ans = usize::MAX;
+    while !current.is_empty() {
+        let mut new = vec![];
 
-    todo!("part1");
-    // panic!("should have an answer")
+        for state in current {
+            if state.is_done() {
+                ans = ans.min(state.score);
+                continue;
+            }
+            for i in 0..8 {
+                if i == state.last_moved {
+                    continue;
+                }
+                let from = state.locations[i];
+                let ignored = ignore_rooms(i);
+                let iscore = score(i);
+                for (to, step, blockers) in available_paths(from) {
+                    if ignored.contains(&to) {
+                        continue;
+                    }
+                    if blockers.into_iter().any(|b| state.locations.contains(&b)) {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        current = new;
+    }
+    ans
 }
 #[cfg(test)]
 mod tests {
@@ -14,16 +43,14 @@ mod tests {
 
     #[fixture]
     pub fn fixture() -> &'static str {
-        r#""#
+        r#"#############
+#...........#
+###B#C#B#D###
+  #A#D#C#A#
+  #########"#
     }
     #[rstest]
     fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
+        assert_eq!(process(fixture), 12521);
     }
 }
