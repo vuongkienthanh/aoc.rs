159a443d7b4cbe67162147790adcb777f1bf2b50
vuongkienthanh <thanhstardust@outlook.com>
2026-02-28 17:33:24 +0700

    ASD

--- 2021/day23/src/lib.rs
+++ 2021/day23/src/lib.rs
@@ -1,396 +1,164 @@
 pub mod part1;
-pub mod part1b;
 pub mod part2;
+use fxhash::FxHashMap;
+use std::ops::Range;
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
+enum Cell {
+    A,
+    B,
+    C,
+    D,
+    E,
+}
+impl From<char> for Cell {
+    fn from(value: char) -> Cell {
+        match value {
+            'A' => Cell::A,
+            'B' => Cell::B,
+            'C' => Cell::C,
+            'D' => Cell::D,
+            _ => panic!(),
+        }
+    }
+}
+impl Cell {
+    fn score(&self) -> usize {
+        match self {
+            Cell::A => 1,
+            Cell::B => 10,
+            Cell::C => 100,
+            Cell::D => 1000,
+            Cell::E => panic!(),
+        }
+    }
+}
+trait Map {
+    fn room(&self, cell: &Cell) -> Range<usize>;
+    fn left(&self, cell: &Cell) -> impl Iterator<Item = usize>;
+    fn right(&self, cell: &Cell) -> impl Iterator<Item = usize>;
+    fn left_hall(&self) -> [usize; 2];
+    fn right_hall(&self) -> [usize; 2];
+    fn mid_hall(&self) -> [usize; 3];
+    fn step(&self, from: usize, to: usize) -> usize;
+    fn obstacle(&self, from: usize, to: usize) -> Vec<usize>;
+    fn get_from(&self, locations: &[Cell]) -> Vec<usize> {
+        let mut ans = vec![];
+        for cell in [Cell::A, Cell::B, Cell::C, Cell::D] {
+            if !self
+                .room(&cell)
+                .all(|x| locations[x] == cell || locations[x] == Cell::E)
+                && let Some(i) = self.room(&cell).rfind(|x| locations[*x] != Cell::E)
+            {
+                ans.push(i);
+            }
+        }
+        for r in [self.left_hall(), self.right_hall()] {
+            if let Some(i) = r.into_iter().find(|x| locations[*x] != Cell::E) {
+                ans.push(i);
+            }
+        }
+        ans.extend(
+            self.mid_hall()
+                .into_iter()
+                .filter(|x| locations[*x] != Cell::E),
+        );
+        ans
+    }
+    fn get_to(&self, locations: &[Cell], from: usize) -> Vec<usize> {
+        let mut ans = vec![];
+        if let Some(cell) = [Cell::A, Cell::B, Cell::C, Cell::D]
+            .into_iter()
+            .find(|x| self.room(x).contains(&from))
+        {
+            for i in self.left(&cell).take_while(|x| locations[*x] == Cell::E) {
+                ans.push(i);
+            }
+            for i in self.right(&cell).take_while(|x| locations[*x] == Cell::E) {
+                ans.push(i);
+            }
+        } else {
+            let cell = locations[from];
+            if self
+                .room(&cell)
+                .all(|x| locations[x] == cell || locations[x] == Cell::E)
+            {
+                let to = self.room(&cell).find(|x| locations[*x] == Cell::E).unwrap();
+                if self
+                    .obstacle(from, to)
+                    .into_iter()
+                    .all(|x| locations[x] == Cell::E)
+                {
+                    ans.push(to);
+                }
+            }
+        }
+
+        ans
+    }
+}
 
 #[derive(Debug)]
-pub struct State {
-    pub locations: [u8; 8],
-    pub last_moved: usize,
-    pub score: usize,
+struct State<const N: usize> {
+    locations: [Cell; N],
+    score: usize,
 }
 
-impl State {
-    pub const SCORE: [usize; 8] = [1, 1, 10, 10, 100, 100, 1000, 1000];
-
-    /// used for hashing and is_done
-    pub fn normalized_locations(&self) -> [u8; 8] {
-        let mut locations = self.locations;
-        if locations[1] < locations[0] {
-            locations.swap(0, 1)
-        }
-        if locations[3] < locations[2] {
-            locations.swap(2, 3)
-        }
-        if locations[5] < locations[4] {
-            locations.swap(4, 5)
-        }
-        if locations[7] < locations[6] {
-            locations.swap(6, 7)
-        }
-
-        locations
+impl<const N: usize> State<N> {
+    const LEN: usize = (N - 7) / 4;
+    fn is_done(&self) -> bool {
+        [Cell::A, Cell::B, Cell::C, Cell::D]
+            .into_iter()
+            .enumerate()
+            .all(|(i, cell)| {
+                self.locations[Self::LEN * i..Self::LEN * (i + 1)]
+                    .iter()
+                    .all(|x| *x == cell)
+            })
     }
-
-    pub fn is_done(&self) -> bool {
-        self.normalized_locations() == [0, 1, 2, 3, 4, 5, 6, 7]
+    fn next(&self, map: &impl Map) -> Vec<State<N>> {
+        let mut ans = vec![];
+        for from in map.get_from(&self.locations) {
+            for to in map.get_to(&self.locations, from) {
+                let mut locations = self.locations;
+                locations.swap(from, to);
+                let score = self.score + map.step(from, to) * locations[to].score();
+                ans.push(State { locations, score });
+            }
+        }
+        ans
     }
+}
 
-    pub fn next_states(&self) -> Vec<State> {
-        // check if any amphipod is already in right places
-        let mut starters = vec![];
-        match &self.locations[0..2] {
-            [0, 1] | [1, 0] => (),
-            [1, _] => starters.push(0),
-            [0, _] => starters.push(1),
-            _ => starters.extend([0, 1]),
-        };
-        match &self.locations[2..4] {
-            [2, 3] | [3, 2] => (),
-            [3, _] => starters.push(2),
-            [2, _] => starters.push(3),
-            _ => starters.extend([2, 3]),
-        };
-        match &self.locations[4..6] {
-            [4, 5] | [5, 4] => (),
-            [5, _] => starters.push(4),
-            [4, _] => starters.push(5),
-            _ => starters.extend([4, 5]),
-        };
-        match &self.locations[6..8] {
-            [6, 7] | [7, 6] => (),
-            [7, _] => starters.push(6),
-            [6, _] => starters.push(7),
-            _ => starters.extend([6, 7]),
-        };
+fn run<const N: usize>(initial_state: State<N>, map: impl Map) -> usize {
+    let mut current = vec![initial_state];
+    let mut seen: FxHashMap<[Cell; N], usize> = FxHashMap::default();
+    seen.insert(current.first().unwrap().locations, 0);
+    let mut ans = usize::MAX;
+    while !current.is_empty() {
+        let mut new = vec![];
 
-        let mut next = vec![];
-
-        for amphipod in starters {
-            // exclude last moved amphipod
-            if amphipod == self.last_moved {
+        for state in current {
+            if state.is_done() {
+                ans = ans.min(state.score);
                 continue;
             }
-            // amphipod should only move to the hallway or their target room
-            let ignored_rooms = match amphipod {
-                0 | 1 => [2, 3, 4, 5, 6, 7],
-                2 | 3 => [0, 1, 4, 5, 6, 7],
-                4 | 5 => [0, 1, 2, 3, 6, 7],
-                6 | 7 => [0, 1, 2, 3, 4, 5],
-                _ => panic!(),
-            };
-            let from = self.locations[amphipod];
-            for (step, pos) in available_paths(from) {
-                // check obstacle
-                if self.locations.iter().any(|lc| pos.contains(lc)) {
-                    continue;
-                }
-                let to = pos.last().cloned().unwrap();
-                // amphipod should only move to the hallway or their target room
-                // so ignore other rooms
-                if ignored_rooms.contains(&to) {
-                    continue;
-                }
-                if [1, 3, 5, 7].contains(&to) {
-                    // move to inner room if possible
-                    if self.locations.iter().all(|lc| *lc != to - 1) {
-                        continue;
+            for next_state in state.next(&map) {
+                match seen.get(&next_state.locations) {
+                    None => {
+                        seen.insert(next_state.locations, next_state.score);
+                        new.push(next_state);
                     }
-                    // when moving to outer room, check if same type is presented
-                    if self.locations[same_type(amphipod)] != to - 1 {
-                        continue;
+                    Some(s) if next_state.score < *s => {
+                        seen.insert(next_state.locations, next_state.score);
+                        new.push(next_state);
                     }
+                    _ => (),
                 }
-
-                let mut locations = self.locations;
-                locations[amphipod] = to;
-                let score = self.score + step * State::SCORE[amphipod];
-                next.push(State {
-                    locations,
-                    last_moved: amphipod,
-                    score,
-                });
             }
         }
 
-        next
+        current = new;
     }
-}
-
-impl From<&str> for State {
-    fn from(value: &str) -> State {
-        fn loc_to_id((r, c): (usize, usize)) -> u8 {
-            match (r, c) {
-                (3, 3) => 0,
-                (2, 3) => 1,
-                (3, 5) => 2,
-                (2, 5) => 3,
-                (3, 7) => 4,
-                (2, 7) => 5,
-                (3, 9) => 6,
-                (2, 9) => 7,
-                _ => panic!(),
-            }
-        }
-        let input = value
-            .lines()
-            .map(|line| line.chars().collect::<Vec<_>>())
-            .collect::<Vec<_>>();
-        let (mut a2, mut b2, mut c2, mut d2) = (false, false, false, false);
-        let mut locations = [0; 8];
-        for (r, c) in [
-            (2, 3),
-            (2, 5),
-            (2, 7),
-            (2, 9),
-            (3, 3),
-            (3, 5),
-            (3, 7),
-            (3, 9),
-        ] {
-            match input[r][c] {
-                'A' if !a2 => {
-                    locations[0] = loc_to_id((r, c));
-                    a2 = true;
-                }
-                'A' if a2 => locations[1] = loc_to_id((r, c)),
-                'B' if !b2 => {
-                    locations[2] = loc_to_id((r, c));
-                    b2 = true;
-                }
-                'B' if b2 => locations[3] = loc_to_id((r, c)),
-                'C' if !c2 => {
-                    locations[4] = loc_to_id((r, c));
-                    c2 = true;
-                }
-                'C' if c2 => locations[5] = loc_to_id((r, c)),
-                'D' if !d2 => {
-                    locations[6] = loc_to_id((r, c));
-                    d2 = true;
-                }
-                'D' if d2 => locations[7] = loc_to_id((r, c)),
-                _ => panic!(),
-            }
-        }
-        State {
-            locations,
-            last_moved: 9,
-            score: 0,
-        }
-    }
-}
-
-// ####1#1#1#11#
-// #89.0.1.2.34#
-// ###1#3#5#7###
-//   #0#2#4#6#
-//   #########
-
-pub fn available_paths(from: u8) -> Vec<(usize, Vec<u8>)> {
-    match from {
-        0 => vec![
-            (6, vec![1, 10, 3, 2]),
-            (5, vec![1, 10, 3]),
-            (8, vec![1, 10, 11, 5, 4]),
-            (7, vec![1, 10, 11, 5]),
-            (10, vec![1, 10, 11, 12, 7, 6]),
-            (9, vec![1, 10, 11, 12, 7]),
-            (4, vec![1, 9, 8]),
-            (3, vec![1, 9]),
-            (3, vec![1, 10]),
-            (5, vec![1, 10, 11]),
-            (7, vec![1, 10, 11, 12]),
-            (9, vec![1, 10, 11, 12, 13]),
-            (10, vec![1, 10, 11, 12, 13, 14]),
-        ],
-        1 => vec![
-            (5, vec![10, 3, 2]),
-            (4, vec![10, 3]),
-            (7, vec![10, 11, 5, 4]),
-            (6, vec![10, 11, 5]),
-            (9, vec![10, 11, 12, 7, 6]),
-            (8, vec![10, 11, 12, 7]),
-            (3, vec![9, 8]),
-            (2, vec![9]),
-            (2, vec![10]),
-            (4, vec![10, 11]),
-            (6, vec![10, 11, 12]),
-            (8, vec![10, 11, 12, 13]),
-            (9, vec![10, 11, 12, 13, 14]),
-        ],
-        2 => vec![
-            (6, vec![3, 10, 1, 0]),
-            (5, vec![3, 10, 1]),
-            (6, vec![3, 11, 5, 4]),
-            (5, vec![3, 11, 5]),
-            (8, vec![3, 11, 12, 7, 6]),
-            (7, vec![3, 11, 12, 7]),
-            (6, vec![3, 10, 9, 8]),
-            (5, vec![3, 10, 9]),
-            (3, vec![3, 10]),
-            (3, vec![3, 11]),
-            (5, vec![3, 11, 12]),
-            (7, vec![3, 11, 12, 13]),
-            (8, vec![3, 11, 12, 13, 13]),
-        ],
-        3 => vec![
-            (5, vec![10, 1, 0]),
-            (4, vec![10, 1]),
-            (5, vec![11, 5, 4]),
-            (4, vec![11, 5]),
-            (7, vec![11, 12, 7, 6]),
-            (6, vec![11, 12, 7]),
-            (5, vec![10, 9, 8]),
-            (4, vec![10, 9]),
-            (2, vec![10]),
-            (2, vec![11]),
-            (4, vec![11, 12]),
-            (6, vec![11, 12, 13]),
-            (7, vec![11, 12, 13, 14]),
-        ],
-        4 => vec![
-            (8, vec![5, 11, 10, 1, 0]),
-            (7, vec![5, 11, 10, 1]),
-            (6, vec![5, 11, 3, 2]),
-            (5, vec![5, 11, 3]),
-            (6, vec![5, 12, 7, 6]),
-            (5, vec![5, 12, 7]),
-            (8, vec![5, 11, 10, 9, 8]),
-            (7, vec![5, 11, 10, 9]),
-            (5, vec![5, 11, 10]),
-            (3, vec![5, 11]),
-            (3, vec![5, 12]),
-            (5, vec![5, 12, 13]),
-            (6, vec![5, 12, 13, 14]),
-        ],
-        5 => vec![
-            (7, vec![11, 10, 1, 0]),
-            (6, vec![11, 10, 1]),
-            (5, vec![11, 3, 2]),
-            (4, vec![11, 3]),
-            (5, vec![12, 7, 6]),
-            (4, vec![12, 7]),
-            (7, vec![11, 10, 9, 8]),
-            (6, vec![11, 10, 9]),
-            (4, vec![11, 10]),
-            (2, vec![11]),
-            (2, vec![12]),
-            (4, vec![12, 13]),
-            (5, vec![12, 13, 14]),
-        ],
-        6 => vec![
-            (10, vec![7, 12, 11, 10, 1, 0]),
-            (9, vec![7, 12, 11, 10, 1]),
-            (8, vec![7, 12, 11, 3, 2]),
-            (7, vec![7, 12, 11, 3]),
-            (6, vec![7, 12, 5, 4]),
-            (5, vec![7, 12, 5]),
-            (10, vec![7, 12, 11, 10, 9, 8]),
-            (9, vec![7, 12, 11, 10, 9]),
-            (7, vec![7, 12, 11, 10]),
-            (5, vec![7, 12, 11]),
-            (3, vec![7, 12]),
-            (3, vec![7, 13]),
-            (4, vec![7, 13, 14]),
-        ],
-        7 => vec![
-            (9, vec![12, 11, 10, 1, 0]),
-            (8, vec![12, 11, 10, 1]),
-            (7, vec![12, 11, 3, 2]),
-            (6, vec![12, 11, 3]),
-            (5, vec![12, 5, 4]),
-            (4, vec![12, 5]),
-            (9, vec![12, 11, 10, 9, 8]),
-            (8, vec![12, 11, 10, 9]),
-            (6, vec![12, 11, 10]),
-            (4, vec![12, 11]),
-            (2, vec![12]),
-            (2, vec![13]),
-            (3, vec![14]),
-        ],
-        8 => vec![
-            (4, vec![9, 1, 0]),
-            (3, vec![9, 1]),
-            (6, vec![9, 10, 3, 2]),
-            (5, vec![9, 10, 3]),
-            (8, vec![9, 10, 11, 5, 4]),
-            (7, vec![9, 10, 11, 5]),
-            (10, vec![9, 10, 11, 12, 7, 6]),
-            (9, vec![9, 10, 11, 12, 7]),
-        ],
-        9 => vec![
-            (3, vec![1, 0]),
-            (2, vec![1]),
-            (5, vec![10, 3, 2]),
-            (4, vec![10, 3]),
-            (7, vec![10, 11, 5, 4]),
-            (6, vec![10, 11, 5]),
-            (9, vec![10, 11, 12, 7, 6]),
-            (8, vec![10, 11, 12, 7]),
-        ],
-        10 => vec![
-            (3, vec![1, 0]),
-            (2, vec![1]),
-            (3, vec![3, 2]),
-            (2, vec![3]),
-            (5, vec![11, 5, 4]),
-            (4, vec![11, 5]),
-            (7, vec![11, 12, 7, 6]),
-            (6, vec![11, 12, 7]),
-        ],
-        11 => vec![
-            (5, vec![10, 1, 0]),
-            (4, vec![10, 1]),
-            (3, vec![3, 2]),
-            (2, vec![3]),
-            (3, vec![5, 4]),
-            (2, vec![5]),
-            (5, vec![12, 7, 6]),
-            (4, vec![12, 7]),
-        ],
-        12 => vec![
-            (7, vec![11, 10, 1, 0]),
-            (6, vec![11, 10, 1]),
-            (5, vec![11, 3, 2]),
-            (4, vec![11, 3]),
-            (3, vec![5, 4]),
-            (2, vec![5]),
-            (3, vec![7, 6]),
-            (2, vec![7]),
-        ],
-        13 => vec![
-            (9, vec![12, 11, 10, 1, 0]),
-            (8, vec![12, 11, 10, 1]),
-            (7, vec![12, 11, 3, 2]),
-            (6, vec![12, 11, 3]),
-            (5, vec![12, 5, 4]),
-            (4, vec![12, 5]),
-            (3, vec![7, 6]),
-            (2, vec![7]),
-        ],
-        14 => vec![
-            (10, vec![13, 12, 11, 10, 1, 0]),
-            (9, vec![13, 12, 11, 10, 1]),
-            (8, vec![13, 12, 11, 3, 2]),
-            (7, vec![13, 12, 11, 3]),
-            (6, vec![13, 12, 5, 4]),
-            (5, vec![13, 12, 5]),
-            (4, vec![13, 7, 6]),
-            (3, vec![13, 7]),
-        ],
-        _ => panic!(),
-    }
-}
-
-fn same_type(a: usize) -> usize {
-    match a {
-        0 => 1,
-        1 => 0,
-        2 => 3,
-        3 => 2,
-        4 => 5,
-        5 => 4,
-        6 => 7,
-        7 => 6,
-        _ => panic!(),
-    }
+    ans
 }
--- 2021/day23/src/part2.rs
+++ 2021/day23/src/part2.rs
@@ -1,4 +1,149 @@
+// #11#1#1#2#22#
+// #67.8.9.0.12#
+// ###3#7#1#5###
+//   #2#6#0#4#
+//   #1#5#9#3#
+//   #0#4#8#2#
+//   #########
+
+use crate::{Cell, Map, State, run};
+use std::ops::Range;
+
+struct Map23;
+impl Map for Map23 {
+    fn room(&self, cell: &Cell) -> Range<usize> {
+        match cell {
+            Cell::A => 0..4,
+            Cell::B => 4..8,
+            Cell::C => 8..12,
+            Cell::D => 12..16,
+            Cell::E => panic!(),
+        }
+    }
+    fn left(&self, cell: &Cell) -> impl Iterator<Item = usize> {
+        match cell {
+            Cell::A => (16..=17).rev(),
+            Cell::B => (16..=18).rev(),
+            Cell::C => (16..=19).rev(),
+            Cell::D => (16..=20).rev(),
+            Cell::E => panic!(),
+        }
+    }
+    fn right(&self, cell: &Cell) -> impl Iterator<Item = usize> {
+        match cell {
+            Cell::A => 18..=22,
+            Cell::B => 19..=22,
+            Cell::C => 20..=22,
+            Cell::D => 21..=22,
+            Cell::E => panic!(),
+        }
+    }
+    fn left_hall(&self) -> [usize; 2] {
+        [17, 16]
+    }
+    fn right_hall(&self) -> [usize; 2] {
+        [21, 22]
+    }
+    fn mid_hall(&self) -> [usize; 3] {
+        [18, 19, 20]
+    }
+    fn step(&self, from: usize, to: usize) -> usize {
+        match (from, to) {
+            (0, 17) | (0, 18) | (4, 18) | (4, 19) | (8, 19) | (8, 20) | (12, 20) | (12, 21) => 5,
+            (0, 19) | (4, 17) | (4, 20) | (8, 18) | (8, 21) | (12, 19) => 7,
+            (0, 20) | (4, 21) | (8, 17) | (12, 18) => 9,
+            (0, 21) | (12, 17) => 11,
+            (x, 16) => self.step(x, 17) + 1,
+            (x, 22) => self.step(x, 21) + 1,
+            (1..=3, y) => self.step(0, y) - from,
+            (5..=7, y) => self.step(4, y) + 4 - from,
+            (9..=11, y) => self.step(8, y) + 8 - from,
+            (13..=15, y) => self.step(12, y) + 12 - from,
+            (x, y) if x >= 16 && y >= 16 => panic!("hall {x} to hall {y}"),
+            (x, y) if x < 16 && y < 16 => panic!("room {x} to room {y}"),
+            (x, y) => self.step(y, x),
+        }
+    }
+    fn obstacle(&self, from: usize, to: usize) -> Vec<usize> {
+        match (from, to) {
+            (17, 0) | (18, 0) | (18, 4) | (19, 4) | (19, 8) | (20, 8) | (20, 12) | (21, 12) => {
+                vec![]
+            }
+            (17, 4) | (19, 0) => vec![18],
+            (17, 8) | (20, 0) => vec![18, 19],
+            (17, 12) | (21, 0) => vec![18, 19, 20],
+            (18, 8) | (20, 4) => vec![19],
+            (18, 12) | (21, 4) => vec![19, 20],
+            (19, 12) | (21, 8) => vec![20],
+            (16, y) => self.obstacle(17, y),
+            (22, y) => self.obstacle(21, y),
+            (x, 1..=3) => self.obstacle(x, 0),
+            (x, 5..=7) => self.obstacle(x, 4),
+            (x, 9..=11) => self.obstacle(x, 8),
+            (x, 13..=15) => self.obstacle(x, 12),
+            (x, y) if x >= 16 && y >= 16 => panic!("hall {x} to hall {y}"),
+            (x, y) if x < 16 && y < 16 => panic!("room {x} to room {y}"),
+            (x, y) => panic!("room {x} to hall {y}"),
+        }
+    }
+}
+impl From<&str> for State<23> {
+    fn from(value: &str) -> State<23> {
+        let input = value
+            .lines()
+            .map(|line| line.chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        let mut locations = [Cell::E; 23];
+        for (r, c, i) in [
+            (3, 3, 0),
+            (2, 3, 3),
+            (3, 5, 4),
+            (2, 5, 7),
+            (3, 7, 8),
+            (2, 7, 11),
+            (3, 9, 12),
+            (2, 9, 15),
+        ] {
+            locations[i] = input[r][c].into();
+        }
+        for (i, cell) in [
+            (1, Cell::D),
+            (2, Cell::D),
+            (5, Cell::B),
+            (6, Cell::C),
+            (9, Cell::A),
+            (10, Cell::B),
+            (13, Cell::C),
+            (14, Cell::A),
+        ] {
+            locations[i] = cell;
+        }
+        State {
+            locations,
+            score: 0,
+        }
+    }
+}
 
 pub fn process(_input: &str) -> usize {
-    todo!("part2");
+    run::<23>(State::from(_input), Map23)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+
+    #[fixture]
+    pub fn fixture() -> &'static str {
+        r#"#############
+#...........#
+###B#C#B#D###
+  #A#D#C#A#
+  #########"#
+    }
+    #[rstest]
+    fn test_process_(fixture: &str) {
+        assert_eq!(process(fixture), 44169);
+    }
 }
