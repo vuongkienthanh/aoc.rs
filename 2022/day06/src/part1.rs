143e0212983b555a207648bb475fa6adee5946d7
vuongkienthanh <thanhstardust@outlook.com>
2026-03-02 13:20:21 +0700

    ASD

--- 2022/day06/src/part1.rs
+++ 2022/day06/src/part1.rs
@@ -1,29 +1,20 @@
-use crate::parsing::parse_input;
-
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
-
-    todo!("part1");
-    // panic!("should have an answer")
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rstest::*;
-
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
-    }
-    #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
-    }
+    _input
+        .lines()
+        .map(|line| {
+            let line: Vec<_> = line.bytes().collect();
+            let mut ans = 0;
+            for (i, c) in line.windows(4).enumerate() {
+                let seen: u32 = 1 << (c[0] - b'a')
+                    | 1 << (c[1] - b'a')
+                    | 1 << (c[2] - b'a')
+                    | 1 << (c[3] - b'a');
+                if seen.count_ones() == 4 {
+                    ans = i + 4;
+                    break;
+                }
+            }
+            ans
+        })
+        .sum()
 }
--- 2022/day06/src/part2.rs
+++ 2022/day06/src/part2.rs
@@ -1,6 +1,21 @@
-use crate::parsing::parse_input;
-
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    todo!("part2");
+    _input
+        .lines()
+        .map(|line| {
+            let line: Vec<_> = line.bytes().collect();
+            let mut ans = 0;
+            for (i, c) in line.windows(14).enumerate() {
+                let seen: u32 = c
+                    .iter()
+                    .map(|x| 1 << (x - b'a'))
+                    .reduce(|a, b| a | b)
+                    .unwrap();
+                if seen.count_ones() == 14 {
+                    ans = i + 14;
+                    break;
+                }
+            }
+            ans
+        })
+        .sum()
 }
