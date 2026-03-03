d4e017716f6bf44b20ee0c3e7229b98967c3de13
vuongkienthanh <54462611+vuongkienthanh@users.noreply.github.com>
2026-03-03 15:26:28 +0700

    d8

--- 2022/day08/src/part1.rs
+++ 2022/day08/src/part1.rs
@@ -1,29 +1,72 @@
-use crate::parsing::parse_input;
+use fxhash::FxHashSet;
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
-
-    todo!("part1");
-    // panic!("should have an answer")
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rstest::*;
-
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
+    let input: Vec<Vec<u8>> = _input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|x| x.to_digit(10).unwrap() as u8)
+                .collect()
+        })
+        .collect();
+    let mut seen = FxHashSet::default();
+    let rows = input.len();
+    let cols = input[0].len();
+    for c in 0..cols {
+        seen.insert((0, c));
+        seen.insert((rows - 1, c));
     }
-    #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
+    for r in 1..rows - 1 {
+        seen.insert((r, 0));
+        seen.insert((r, cols - 1));
     }
 
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
+    for r in 1..rows - 1 {
+        let row = input.get(r).unwrap();
+        let mut left_max = row[0];
+        for (c, cell) in row.iter().enumerate() {
+            if *cell > left_max {
+                seen.insert((r, c));
+                left_max = *cell;
+                if left_max == 9 {
+                    break;
+                }
+            }
+        }
+        let mut right_max = row.last().cloned().unwrap();
+        for (c, cell) in row.iter().enumerate().rev() {
+            if *cell > right_max {
+                seen.insert((r, c));
+                right_max = *cell;
+                if right_max == 9 {
+                    break;
+                }
+            }
+        }
     }
+    for c in 1..cols - 1 {
+        let col: Vec<_> = input.iter().map(|r| r.get(c).unwrap()).cloned().collect();
+        let mut top_max = col[0];
+        for (r, cell) in col.iter().enumerate() {
+            if *cell > top_max {
+                seen.insert((r, c));
+                top_max = *cell;
+                if top_max == 9 {
+                    break;
+                }
+            }
+        }
+        let mut bottom_max = col.last().cloned().unwrap();
+        for (r, cell) in col.iter().enumerate().rev() {
+            if *cell > bottom_max {
+                seen.insert((r, c));
+                bottom_max = *cell;
+                if bottom_max == 9 {
+                    break;
+                }
+            }
+        }
+    }
+
+    seen.len()
 }
--- 2022/day08/src/part2.rs
+++ 2022/day08/src/part2.rs
@@ -1,6 +1,38 @@
-use crate::parsing::parse_input;
-
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    todo!("part2");
+    let input: Vec<Vec<u8>> = _input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|x| x.to_digit(10).unwrap() as u8)
+                .collect()
+        })
+        .collect();
+    let rows = input.len();
+    let cols = input[0].len();
+    let mut ans = 0;
+    for r in 0..rows {
+        for c in 0..cols {
+            let center = input[r][c];
+            let left = (0..c)
+                .rev()
+                .enumerate()
+                .find_map(|(i, c)| (input[r][c] >= center).then_some(i + 1))
+                .unwrap_or(c);
+            let right = (c + 1..cols)
+                .enumerate()
+                .find_map(|(i, c)| (input[r][c] >= center).then_some(i + 1))
+                .unwrap_or(cols - c-1);
+            let top = (0..r)
+                .rev()
+                .enumerate()
+                .find_map(|(i, r)| (input[r][c] >= center).then_some(i + 1))
+                .unwrap_or(r);
+            let bottom = (r + 1..rows)
+                .enumerate()
+                .find_map(|(i, r)| (input[r][c] >= center).then_some(i + 1))
+                .unwrap_or(rows - r -1);
+            ans = ans.max(top * bottom * left * right);
+        }
+    }
+    ans
 }
