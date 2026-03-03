8f23ecf607ac3fcd201722837e5c82eff06331c9
vuongkienthanh <54462611+vuongkienthanh@users.noreply.github.com>
2026-03-03 11:46:15 +0700

    z

--- 2022/day07/src/lib.rs
+++ 2022/day07/src/lib.rs
@@ -15,8 +15,65 @@ pub enum CD<'a> {
     Path(&'a str),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]]
 pub enum LS<'a> {
     Dir(&'a str),
     File(usize),
 }
+
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+struct Node<'a> {
+    data: LS<'a>,
+    parent: Option<Weak<RefCell<Node<'a>>>>,
+    children: Vec<Rc<RefCell<Node<'a>>>>,
+}
+
+fn build_tree(input: Vec<CMD>) -> Rc<RefCell<Node>> {
+    let root = Rc::new(RefCell::new(Node {
+        data: LS::Dir("/"),
+        parent: None,
+        children: vec![],
+    }));
+    let mut current_node = Rc::clone(&root);
+    for cmd in input {
+        match cmd {
+            CMD::cd(CD::Back) => {
+                let mut parent = None;
+                if let Some(p) = &current_node.borrow().parent {
+                    parent = Some(p.clone());
+                }
+                if let Some(p) = parent {
+                    current_node = p.upgrade().unwrap();
+                }
+            }
+            CMD::cd(CD::Path(p)) => {
+                let target = current_node
+                    .borrow()
+                    .children
+                    .iter()
+                    .find(|n| n.borrow().data == LS::Dir(p))
+                    .map(|c| Rc::clone(c))
+                    .unwrap();
+                current_node = target;
+            }
+            CMD::ls(v) => {
+                for child in v {
+                    let child = Node {
+                        data: child,
+                        parent: Some(Rc::downgrade(&current_node)),
+                        children: vec![],
+                    };
+                    current_node
+                        .borrow_mut()
+                        .children
+                        .push(Rc::new(RefCell::new(child)));
+                }
+            }
+        }
+    }
+    root
+}
+
--- 2022/day07/src/part1.rs
+++ 2022/day07/src/part1.rs
@@ -8,3 +8,20 @@ pub fn process(_input: &str) -> usize {
     todo!("part1");
     // panic!("should have an answer")
 }
+fn get_size(node: &Rc<RefCell<Node>>, ans: &mut usize) -> usize {
+    match node.borrow().data {
+        LS::Dir(_) => {
+            let size = node
+                .borrow()
+                .children
+                .iter()
+                .map(|x| get_size(x, ans))
+                .sum::<usize>();
+            if size <= 100_000 {
+                *ans += size;
+            }
+            *ans
+        }
+        LS::File(u) => u,
+    }
+}
