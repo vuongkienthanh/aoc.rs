3b3be75fb16d5e225de7b6be0e133e33d0ff0917
vuongkienthanh <thanhstardust@outlook.com>
2026-03-09 15:52:50 +0700

    ASD

--- 2022/day16/src/parsing.rs
+++ 2022/day16/src/parsing.rs
@@ -1,25 +1,35 @@
-#[allow(unused_imports)]
-// use aoc_helper::nom::parse_signed_usize;
+use fxhash::FxHashMap;
 use nom::{
+    IResult, Parser,
     branch::alt,
     bytes::complete::tag,
     character::complete::{self, alpha1, line_ending},
     combinator::all_consuming,
     multi::separated_list1,
     sequence::{delimited, preceded, separated_pair, terminated},
-    IResult, Parser,
 };
-// https://github.com/rust-bakery/nom/blob/main/doc/choosing_a_combinator.md
 
-type Item = usize;
-
-fn parse_line(input: &str) -> IResult<&str, Item> {
-    todo!()
+fn parse_line(input: &str) -> IResult<&str, (&str, usize, Vec<&str>)> {
+    (
+        preceded(tag("Valve "), alpha1),
+        preceded(tag(" has flow rate="), complete::usize),
+        preceded(
+            (
+                alt((tag("; tunnels lead to "), tag("; tunnel leads to "))),
+                alt((tag("valves "), tag("valve "))),
+            ),
+            separated_list1(tag(", "), alpha1),
+        ),
+    )
+        .parse(input)
 }
 
-pub fn parse_input(input: &str) -> Vec<Item> {
+pub fn parse_input(input: &str) -> FxHashMap<&str, (usize, Vec<&str>)> {
     all_consuming(separated_list1(line_ending, parse_line))
         .parse(input)
         .unwrap()
         .1
+        .into_iter()
+        .map(|(from, rate, to)| (from, (rate, to)))
+        .collect()
 }
--- 2022/day16/src/part1.rs
+++ 2022/day16/src/part1.rs
@@ -1,12 +1,31 @@
 use crate::parsing::parse_input;
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
-
-    todo!("part1");
-    // panic!("should have an answer")
+    let map = parse_input(_input);
+    // println!("{input:?}");
+    let mut max = 0;
+    let mut current = vec![(vec!["AA"], 0)];
+    // for i in (2..=30).step_by(2) {
+    //     let mut new = vec![];
+    //
+    //     for (path, mut pressure) in current {
+    //         let last = path.last().unwrap();
+    //         let (last_pressure, last_path) = map.get(last).unwrap();
+    //         pressure += last_pressure * (32 - i);
+    //         max = max.max(pressure);
+    //         for next_loc in last_path {
+    //             let mut path = path.clone();
+    //             path.push(next_loc);
+    //             new.push((path, pressure));
+    //         }
+    //     }
+    //
+    //     current = new;
+    //     println!("{}", current.len());
+    // }
+    max
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,16 +33,19 @@ mod tests {
 
     #[fixture]
     pub fn fixture() -> &'static str {
-        r#""#
+        r#"Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II"#
     }
     #[rstest]
     fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
+        assert_eq!(process(fixture), 1651);
     }
 }
