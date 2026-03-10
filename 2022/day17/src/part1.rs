fea513d498e3b9458feae78fdae25ea07cb4b7a6
vuongkienthanh <54462611+vuongkienthanh@users.noreply.github.com>
2026-03-10 11:41:58 +0700

    asd

--- 2022/day17/src/part1.rs
+++ 2022/day17/src/part1.rs
@@ -1,12 +1,73 @@
-use crate::parsing::parse_input;
+//! treat up as right, down as left
+
+const ROCKS: [[[char; 7]; 4]; 5] = [
+    [
+        ['.', '.', '#', '#', '#', '#', '.'],
+        ['.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.'],
+    ],
+    [
+        ['.', '.', '.', '#', '.', '.', '.'],
+        ['.', '.', '#', '#', '#', '.', '.'],
+        ['.', '.', '.', '#', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.'],
+    ],
+    [
+        ['.', '.', '#', '#', '#', '.', '.'],
+        ['.', '.', '#', '.', '.', '.', '.'],
+        ['.', '.', '#', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.'],
+    ],
+    [
+        ['.', '.', '.', '.', '#', '.', '.'],
+        ['.', '.', '.', '.', '#', '.', '.'],
+        ['.', '.', '.', '.', '#', '.', '.'],
+        ['.', '.', '.', '.', '#', '.', '.'],
+    ],
+    [
+        ['.', '.', '.', '#', '#', '.', '.'],
+        ['.', '.', '.', '#', '#', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.', '.'],
+    ],
+];
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
+    let mut hole: Vec<[char; 7]> = vec![['.'; 7]; 3];
+    let mut rocks = ROCKS.iter().cycle();
+    for i in 0..2 {
+        if let Some(last_not_empty) = hole
+            .iter()
+            .rev()
+            .enumerate()
+            .find_map(|(i, row)| (!row.iter().all(|x| *x == '.')).then_some(i))
+        {
+            hole.extend(vec![['.'; 7]; 3 - last_not_empty]);
+        }
+        let rock = rocks.next().unwrap();
+        hole.extend(rock);
 
-    todo!("part1");
-    // panic!("should have an answer")
+        println!("{i}");
+        for row in hole.iter().rev() {
+            println!("{row:?}");
+        }
+    }
+    
+    todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process_() {
+        let _input = r#">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"#;
+        assert_eq!(process(_input), 3068);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,16 +75,11 @@ mod tests {
 
     #[fixture]
     pub fn fixture() -> &'static str {
-        r#""#
+        r#">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"#
     }
+    
     #[rstest]
     fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
+        assert_eq!(process(fixture), 3068);
     }
 }
