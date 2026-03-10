8e3316022b6199e4c9c8942389820a4b03d0007b
vuongkienthanh <thanhstardust@outlook.com>
2026-03-10 13:49:48 +0700

    ASD

--- 2022/day17/src/part1.rs
+++ 2022/day17/src/part1.rs
@@ -1,71 +1,85 @@
-//! treat up as right, down as left
-
-const ROCKS: [[[char; 7]; 4]; 5] = [
-    [
-        ['.', '.', '#', '#', '#', '#', '.'],
-        ['.', '.', '.', '.', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.', '.'],
-    ],
-    [
-        ['.', '.', '.', '#', '.', '.', '.'],
-        ['.', '.', '#', '#', '#', '.', '.'],
-        ['.', '.', '.', '#', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.', '.'],
-    ],
-    [
-        ['.', '.', '#', '#', '#', '.', '.'],
-        ['.', '.', '#', '.', '.', '.', '.'],
-        ['.', '.', '#', '.', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.', '.'],
-    ],
-    [
-        ['.', '.', '.', '.', '#', '.', '.'],
-        ['.', '.', '.', '.', '#', '.', '.'],
-        ['.', '.', '.', '.', '#', '.', '.'],
-        ['.', '.', '.', '.', '#', '.', '.'],
-    ],
-    [
-        ['.', '.', '.', '#', '#', '.', '.'],
-        ['.', '.', '.', '#', '#', '.', '.'],
-        ['.', '.', '.', '.', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.', '.'],
-    ],
+const ROCKS: [[u8; 4]; 5] = [
+    [0b11110, 0, 0, 0],
+    [0b01000, 0b11100, 0b01000, 0],
+    [0b11100, 0b00100, 0b00100, 0],
+    [0b10000, 0b10000, 0b10000, 0b10000],
+    [0b11000, 0b11000, 0, 0],
 ];
 
-pub fn process(_input: &str) -> usize {
-    let mut hole: Vec<[char; 7]> = vec![['.'; 7]; 3];
-    let mut rocks = ROCKS.iter().cycle();
-    for i in 0..2 {
-        if let Some(last_not_empty) = hole
-            .iter()
-            .rev()
-            .enumerate()
-            .find_map(|(i, row)| (!row.iter().all(|x| *x == '.')).then_some(i))
-        {
-            hole.extend(vec![['.'; 7]; 3 - last_not_empty]);
-        }
-        let rock = rocks.next().unwrap();
-        hole.extend(rock);
-
-        println!("{i}");
-        for row in hole.iter().rev() {
-            println!("{row:?}");
-        }
+fn move_left(rock: [u8; 4]) -> [u8; 4] {
+    if rock.iter().all(|x| x & (1 << 6) == 0) {
+        [rock[0] << 1, rock[1] << 1, rock[2] << 1, rock[3] << 1]
+    } else {
+        rock
     }
-    
-    todo!()
+}
+fn move_right(rock: [u8; 4]) -> [u8; 4] {
+    if rock.iter().all(|x| x & 1 == 0) {
+        [rock[0] >> 1, rock[1] >> 1, rock[2] >> 1, rock[3] >> 1]
+    } else {
+        rock
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn process(_input: &str) -> usize {
+    let mut hole: Vec<u8> = vec![];
+    let mut rocks = ROCKS.iter().cycle();
+    let mut cmds = _input.chars().cycle();
+    for i in 0..2 {
+        println!("{i}");
+        let len = hole.len();
+        let mut rock = rocks.next().cloned().unwrap();
+        //move rock 3 times and down 3 times
+        for _ in 0..3 {
+            match cmds.next().unwrap() {
+                '>' => rock = move_right(rock),
+                '<' => rock = move_left(rock),
+                _ => panic!(),
+            }
+        }
+        // clash
+        if len == 0 {
+            match cmds.next().unwrap() {
+                '>' => rock = move_right(rock),
+                '<' => rock = move_left(rock),
+                _ => panic!(),
+            }
+            hole.extend(rock);
+        } else {
+            for i in (len.saturating_sub(4)..len).rev() {
+                let clash_area = &hole[i..len];
+                let moved_rock = match cmds.next().unwrap() {
+                    '>' => move_right(rock),
+                    '<' => move_left(rock),
+                    _ => panic!(),
+                };
 
-    #[test]
-    fn test_process_() {
-        let _input = r#">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"#;
-        assert_eq!(process(_input), 3068);
+                let rock_after_cmd = if clash_area.iter().zip(moved_rock).all(|(a, b)| {
+                    let fuse = a | b;
+                    fuse.count_ones() == a.count_ones() + b.count_ones()
+                }) {
+                    moved_rock
+                } else {
+                    rock
+                };
+            }
+        }
+
+        while let Some(row) = hole.pop() {
+            if row != 0 {
+                hole.push(row);
+                break;
+            }
+        }
+
+        println!("hole = ");
+        for row in &hole {
+            println!("{row:07b}");
+        }
+        println!();
     }
+
+    todo!()
 }
 
 #[cfg(test)]
@@ -77,7 +91,7 @@ mod tests {
     pub fn fixture() -> &'static str {
         r#">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"#
     }
-    
+
     #[rstest]
     fn test_process_(fixture: &str) {
         assert_eq!(process(fixture), 3068);
