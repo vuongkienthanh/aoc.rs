42a595e4f78ea6d7c61727bd0f20df7e0d103f91
vuongkienthanh <54462611+vuongkienthanh@users.noreply.github.com>
2026-03-16 15:49:56 +0700

    tx

--- 2022/day19/src/lib.rs
+++ 2022/day19/src/lib.rs
@@ -1,3 +1,64 @@
 pub mod part1;
 pub mod part2;
 pub mod parsing;
+
+type Robots = (usize, usize, usize, usize);
+type Resource = (usize, usize, usize, usize);
+
+#[derive(Debug)]
+struct Blueprint {
+    id: usize,
+    ore_robot_need: usize,
+    clay_robot_need: usize,
+    obsidian_robot_need: (usize, usize),
+    geode_robot_need: (usize, usize),
+}
+impl Blueprint {
+    fn make_robot(&self, (a, b, c, d): Robots, resource: &Resource) -> Option<(Robots, Resource)> {
+        if resource.ore >= self.geode_robot_need.0 && resource.obsidian >= self.geode_robot_need.1 {
+            Some((
+                (a, b, c, d + 1),
+                Resource {
+                    ore: resource.ore - self.geode_robot_need.0,
+                    clay: resource.clay,
+                    obsidian: resource.obsidian - self.geode_robot_need.1,
+                    geode: resource.geode,
+                },
+            ));
+        } else if resource.ore >= self.obsidian_robot_need.0
+            && resource.clay >= self.obsidian_robot_need.1
+        {
+            Some((
+                (a, b, c + 1, d),
+                Resource {
+                    ore: resource.ore - self.obsidian_robot_need.0,
+                    clay: resource.clay - self.obsidian_robot_need.1,
+                    obsidian: resource.obsidian,
+                    geode: resource.geode,
+                },
+            ));
+        } else if resource.ore >= self.clay_robot_need {
+            Some((
+                (a, b + 1, c, d),
+                Resource {
+                    ore: resource.ore - self.clay_robot_need,
+                    clay: resource.clay,
+                    obsidian: resource.obsidian,
+                    geode: resource.geode,
+                },
+            ));
+        } else if resource.ore >= self.ore_robot_need {
+            Some((
+                (a + 1, b, c, d),
+                Resource {
+                    ore: resource.ore - self.ore_robot_need,
+                    clay: resource.clay,
+                    obsidian: resource.obsidian,
+                    geode: resource.geode,
+                },
+            ));
+        } else {
+            None
+        }
+    }
+}
--- 2022/day19/src/parsing.rs
+++ 2022/day19/src/parsing.rs
@@ -1,24 +1,41 @@
-#[allow(unused_imports)]
-// use aoc_helper::nom::parse_signed_usize;
 use nom::{
-    branch::alt,
+    IResult, Parser,
     bytes::complete::tag,
-    character::complete::{self, alpha1, line_ending},
+    character::complete::{self, line_ending},
     combinator::all_consuming,
     multi::separated_list1,
-    sequence::{delimited, preceded, separated_pair, terminated},
-    IResult, Parser,
 };
-// https://github.com/rust-bakery/nom/blob/main/doc/choosing_a_combinator.md
 
-type Item = usize;
-
-fn parse_line(input: &str) -> IResult<&str, Item> {
-    todo!()
+fn parse_blueprint(input: &str) -> IResult<&str, Blueprint> {
+    (
+        tag("Blueprint "),
+        complete::usize,
+        tag(": Each ore robot costs "),
+        complete::usize,
+        tag(" ore. Each clay robot costs "),
+        complete::usize,
+        tag(" ore. Each obsidian robot costs "),
+        complete::usize,
+        tag(" ore and "),
+        complete::usize,
+        tag(" clay. Each geode robot costs "),
+        complete::usize,
+        tag(" ore and "),
+        complete::usize,
+        tag(" obsidian."),
+    )
+        .map(|(_, i, _, a, _, b, _, c, _, d, _, e, _, f, _)| Blueprint {
+            id: i,
+            ore_robot_need: a,
+            clay_robot_need: b,
+            obsidian_robot_need: (c, d),
+            geode_robot_need: (e, f),
+        })
+        .parse(input)
 }
 
-pub fn parse_input(input: &str) -> Vec<Item> {
-    all_consuming(separated_list1(line_ending, parse_line))
+pub fn parse_input(input: &str) -> Vec<Blueprint> {
+    all_consuming(separated_list1(line_ending, parse_blueprint))
         .parse(input)
         .unwrap()
         .1
