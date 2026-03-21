1450eb1553d9c18b799b05a7d51af77347ee10e5
vuongkienthanh <54462611+vuongkienthanh@users.noreply.github.com>
2026-03-21 11:01:03 +0700

    asd

--- 2022/day22/src/parsing.rs
+++ 2022/day22/src/parsing.rs
@@ -1,25 +1,58 @@
-#[allow(unused_imports)]
-// use aoc_helper::nom::parse_signed_usize;
 use nom::{
+    IResult, Parser,
     branch::alt,
     bytes::complete::tag,
-    character::complete::{self, alpha1, line_ending},
+    character::complete::{self, line_ending},
     combinator::all_consuming,
-    multi::separated_list1,
-    sequence::{delimited, preceded, separated_pair, terminated},
-    IResult, Parser,
+    multi::{many1, separated_list1},
+    sequence::separated_pair,
 };
-// https://github.com/rust-bakery/nom/blob/main/doc/choosing_a_combinator.md
+use aoc_helper::direction::Direction;
 
-type Item = usize;
-
-fn parse_line(input: &str) -> IResult<&str, Item> {
-    todo!()
+#[derive(Debug, Clone, Default)]
+pub enum Cell {
+    #[default]
+    Empty,
+    Wall,
+    Space,
 }
 
-pub fn parse_input(input: &str) -> Vec<Item> {
-    all_consuming(separated_list1(line_ending, parse_line))
-        .parse(input)
-        .unwrap()
-        .1
+#[derive(Debug)]
+pub enum Op {
+    Mv(usize),
+    L,
+    R,
+}
+
+fn parse_map_line(input: &str) -> IResult<&str, Vec<Cell>> {
+    many1(alt((
+        tag(" ").map(|_| Cell::Empty),
+        tag(".").map(|_| Cell::Space),
+        tag("#").map(|_| Cell::Wall),
+    )))
+    .parse(input)
+}
+
+fn parse_map(input: &str) -> IResult<&str, Vec<Vec<Cell>>> {
+    separated_list1(line_ending, parse_map_line).parse(input)
+}
+
+fn parse_ops(input: &str) -> IResult<&str, Vec<Op>> {
+    many1(alt((
+        complete::usize.map(Op::Mv),
+        tag("L").map(|_| Op::L),
+        tag("R").map(|_| Op::R),
+    )))
+    .parse(input)
+}
+
+pub fn parse_input(input: &str) -> (Vec<Vec<Cell>>, Vec<Op>) {
+    all_consuming(separated_pair(
+        parse_map,
+        (line_ending, line_ending),
+        parse_ops,
+    ))
+    .parse(input)
+    .unwrap()
+    .1
 }
--- 2022/day22/src/part1.rs
+++ 2022/day22/src/part1.rs
@@ -1,12 +1,99 @@
 use crate::parsing::parse_input;
+use aoc_helper::direction::Direction;
 
 pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
+    let (map, ops) = parse_input(_input);
+    let mut start: (usize, usize, Direction) = map
+        .first()
+        .unwrap()
+        .iter()
+        .enumerate()
+        .find_map(|(i, c)| matches!(c, Cell::Space).then_some((0, i, Direction::Right)))
+        .unwrap();
+    // for line in &map {
+    //     for cell in line {
+    //         match cell {
+    //             Cell::Empty => print!(" "),
+    //             Cell::Wall => print!("#"),
+    //             Cell::Space => print!("."),
+    //         }
+    //     }
+    //     println!();
+    // }
+    // println!("{ops:?}");
+    for op in ops {
+        match op {
+            Op::L => start.2 = start.2.turn_left(),
+            Op::R => start.2 = start.2.turn_right(),
+            Op::Mv(i) => match start.2 {
+                Direction::Up => {
+                    for _ in 0..i {
+                        let target_r = (start.0 + map.len() - 1) % map.len();
+                        match map.get(target_r).unwrap().get(start.1).cloned().unwrap_or_default() {
+                            Cell::Empty => {
+                                let (target_r, cell) = map
+                                    .iter()
+                                    .enumerate()
+                                    .skip(start.0)
+                                    .map(|(r, row)| {
+                                        (r, row.get(start.1).cloned().unwrap_or_default())
+                                    })
+                                    .take_while(|(_, cell)| !matches!(cell, Cell::Empty))
+                                    .last()
+                                    .unwrap();
+                                match cell {
+                                    Cell::Empty => panic!(),
+                                    Cell::Space => start.0 = target_r,
+                                    Cell::Wall => break,
+                                }
+                            }
+                            Cell::Space => start.0 = target_r,
+                            Cell::Wall => break,
+                        }
+                    }
+                }
+                Direction::Down => {}
+                Direction::Left => {
+                    for _ in 0..i {
+                        let len = map.get(start.0).unwrap().len();
+                        let target_c = (start.1 + len - 1) % len;
+                        match map.get(start.0).unwrap().get(target_c).unwrap() {
+                            Cell::Empty => {
+                                let (target_c, cell) = map
+                                    .get(start.0)
+                                    .unwrap()
+                                    .iter()
+                                    .enumerate()
+                                    .skip(start.1)
+                                    .take_while(|(_, cell)| !matches!(cell, Cell::Empty))
+                                    .last()
+                                    .unwrap();
+                                match cell {
+                                    Cell::Empty => panic!(),
+                                    Cell::Space => start.1 = target_c,
+                                    Cell::Wall => break,
+                                }
+                            }
+                            Cell::Space => start.1 = target_c,
+                            Cell::Wall => break,
+                        }
+                    }
+                }
+                Direction::Right => {}
+            },
+        }
+    }
 
-    todo!("part1");
-    // panic!("should have an answer")
+    (start.0 + 1) * 1000
+        + (start.1 + 1) * 4
+        + match start.2 {
+            Direction::Right => 0,
+            Direction::Down => 1,
+            Direction::Left => 2,
+            Direction::Up => 3,
+        }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,16 +101,24 @@ mod tests {
 
     #[fixture]
     pub fn fixture() -> &'static str {
-        r#""#
-    }
-    #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
+        r#"        ...#
+        .#..
+        #...
+        ....
+...#.......#
+........#...
+..#....#....
+..........#.
+        ...#....
+        .....#..
+        .#......
+        ......#.
+
+10R5L5R10L4R5L5"#
     }
 
     #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
+    fn test_process_(fixture: &str) {
+        assert_eq!(process(fixture), 6032);
     }
 }
