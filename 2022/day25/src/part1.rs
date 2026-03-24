cbcaf92d6f5914b4f26e606d7cc4b45711843ba3
vuongkienthanh <54462611+vuongkienthanh@users.noreply.github.com>
2026-03-24 13:00:10 +0700

    d25

--- 2022/day25/src/part1.rs
+++ 2022/day25/src/part1.rs
@@ -1,29 +1,49 @@
-use crate::parsing::parse_input;
-
-pub fn process(_input: &str) -> usize {
-    let input = parse_input(_input);
-    println!("{input:?}");
-
-    todo!("part1");
-    // panic!("should have an answer")
+fn decimal(input: &str) -> isize {
+    input
+        .chars()
+        .rev()
+        .map(|c| match c {
+            '2' => 2,
+            '1' => 1,
+            '0' => 0,
+            '-' => -1,
+            '=' => -2,
+            _ => panic!(),
+        })
+        .enumerate()
+        .map(|(i, c)| c * 5isize.pow(i as u32))
+        .sum()
 }
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rstest::*;
 
-    #[fixture]
-    pub fn fixture() -> &'static str {
-        r#""#
+fn SNAFU(mut input: isize) -> String {
+    let mut ans = String::new();
+    while input > 0 {
+        match input % 5 {
+            0 => ans.push('0'),
+            1 => {
+                ans.push('1');
+                input -= 1;
+            }
+            2 => {
+                ans.push('2');
+                input -= 2;
+            }
+            3 => {
+                ans.push('=');
+                input += 2;
+            }
+            4 => {
+                ans.push('-');
+                input += 1;
+            }
+            _ => (),
+        }
+        input /= 5;
     }
-    #[rstest]
-    fn test_process_(fixture: &str) {
-        assert_eq!(process(fixture), 0);
-    }
+    ans.chars().rev().collect()
+}
 
-    #[rstest]
-    #[case("", 0)]
-    fn test_process(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(process(input), expected);
-    }
+pub fn process(_input: &str) -> String {
+    let s = _input.lines().map(|line| decimal(line)).sum();
+    SNAFU(s)
 }
